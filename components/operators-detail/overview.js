5e795b6e7cad3368ddc41492a3c85a5dc17622c8
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-06-02 09:46:33 +0200

    Operators: detail overview

--- components/operators-detail/overview.js
+++ components/operators-detail/overview.js
@@ -10,6 +10,18 @@ export default function OperatorsDetailOverview() {
     >
       <div className="l-container">
         <Gallery1 />
+        <article className="c-article">
+          <div className="row custom-row">
+            <div className="columns small-12 medium-8">
+              <header>
+                <h2 className="c-title">Overview</h2>
+              </header>
+              <div className="description">
+                <p>REM is a non-profit organisation that operates as Independent Monitor of Law Enforcement and Governance. Our mission is to stimulate government reform and action in natural resource extraction through independent monitoring and credible reporting of illegalities and related governance problems. We use this information to develop, with the concerned actors, constructive and viable solutions and assist in their implementation.</p>
+              </div>
+            </div>
+          </div>
+        </article>
       </div>
     </div>
   );
