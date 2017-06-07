65c961d0d3aa25fe1ceedd9133cbead948eeac9c
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-06-07 16:04:53 +0200

    Operators: operator detail documentation

--- components/operators-detail/documentation.js
+++ components/operators-detail/documentation.js
@@ -3,12 +3,13 @@ import groupBy from 'lodash/groupBy';
 
 // Constants
 import { TABS_DOCUMENTATION_OPERATORS_DETAIL, DOCUMENTATION_OPERATORS_DETAIL } from 'constants/operators-detail';
-import { PALETTE_COLOR_2, ANIMATION_TIMES } from 'constants/rechart';
+import { PALETTE_COLOR_2, ANIMATION_TIMES, LEGEND_DOCUMENTATION } from 'constants/rechart';
 
 // Components
-import { PieChart, Pie, ResponsiveContainer, Cell } from 'recharts';
+import { PieChart, Pie, ResponsiveContainer, Cell, Legend } from 'recharts';
 import StaticTabs from 'components/ui/static-tabs';
 import DocCard from 'components/ui/doc-card';
+import ChartLegend from 'components/ui/chart-legend';
 
 const exampleData = [
   { name: 'Not provided', value: 4, fill: PALETTE_COLOR_2[0].fill, stroke: PALETTE_COLOR_2[0].stroke },
@@ -47,23 +48,34 @@ export default class OperatorsDetailDocumentation extends React.Component {
 
               <div className="row custom-row">
                 <div className="columns small-6">
-                  <ResponsiveContainer height={350}>
-                    <PieChart>
-                      <Pie
-                        data={exampleData}
-                        dataKey="value"
-                        outerRadius={170}
-                        innerRadius={160}
-                        startAngle={90}
-                        endAngle={-270}
-                        {...ANIMATION_TIMES}
-                      >
-                        {exampleData.map(entry =>
-                          <Cell key={entry.name} fill={entry.fill} stroke={entry.stroke} />
-                        )}
-                      </Pie>
-                    </PieChart>
-                  </ResponsiveContainer>
+                  <div className="c-chart">
+                    <ResponsiveContainer height={350}>
+                      <PieChart>
+                        <Pie
+                          data={exampleData}
+                          dataKey="value"
+                          outerRadius={170}
+                          innerRadius={162}
+                          startAngle={90}
+                          endAngle={-270}
+                          {...ANIMATION_TIMES}
+                        >
+                          {exampleData.map(entry =>
+                            <Cell key={entry.name} fill={entry.fill} stroke={entry.stroke} />
+                          )}
+                        </Pie>
+                      </PieChart>
+                    </ResponsiveContainer>
+
+                    {/* Legend */}
+                    <ChartLegend
+                      list={LEGEND_DOCUMENTATION.list}
+                      className="-absolute"
+                    />
+                  </div>
+                </div>
+
+                <div className="columns small-6">
                 </div>
               </div>
             </article>
