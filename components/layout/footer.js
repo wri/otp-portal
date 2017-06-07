14dce5bbb28f9410cc9def712d181c2b3b5d788e
bitTal <albafjez@gmail.com>
2017-06-07 12:44:45 +0200

    Footer component

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 export default() => (
-  <footer>
-    {/* Footer */}
+  <footer className="c-footer">
+    <div className="l-container">
+      <div className="footer-container">
+        <div className="footer-item">
+          <a href="http://www.wri.org/" target="_blank" rel="noreferrer noopener">
+            <img src="/static/images/logos/wri-logo.svg" alt="WRI logo" />
+          </a>
+        </div>
+        <div className="footer-item">
+          <p className="-bold">World Resources Institute</p>
+          <p>10 G Street NE Suite 800, Washington, DC 20002, USA</p>
+          <p>Phone +1(202) 729-7600    |    Fax: +1 (202) 720 7610</p>
+        </div>
+      </div>
+    </div>
   </footer>
 );
