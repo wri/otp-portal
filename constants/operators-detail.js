9d046f41d229cdc2f5437d6ab7ea496e4c96ea15
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-06-08 16:35:00 +0200

    Operators: detail observations

--- constants/operators-detail.js
+++ constants/operators-detail.js
@@ -28,6 +28,151 @@ const TABS_DOCUMENTATION_OPERATORS_DETAIL = [{
 }];
 
 
+
+const MAP_OPTIONS_OPERATORS_DETAIL = {
+  zoom: 5,
+  center: [18, 0],
+  scrollZoom: false
+};
+
+const MAP_LAYERS_OPERATORS_DETAIL = [
+  {
+    id: 'forest_concession',
+    provider: 'cartodb',
+    source: {
+      type: 'geojson',
+      data: `https://simbiotica.carto.com/api/v2/sql?q=${encodeURIComponent('SELECT * FROM forest_concession')}&format=geojson`
+    },
+    layers: [{
+      id: 'forest_concession_layer_hover',
+      type: 'fill',
+      source: 'forest_concession',
+      layout: {},
+      paint: {
+        'fill-color': '#d07500',
+        'fill-opacity': 0.8,
+        'fill-outline-color': '#d07500'
+      },
+      filter: ['==', 'cartodb_id', '']
+    }, {
+      id: 'forest_concession_layer',
+      type: 'fill',
+      source: 'forest_concession',
+      layout: {},
+      paint: {
+        'fill-color': '#e98300',
+        'fill-opacity': 0.8,
+        'fill-outline-color': '#d07500'
+      },
+      interactivity: {
+        click(e) {
+          // Remove always the popup if exists and you are using 'closeOnClick'
+          // You will prevent a bug that doesn't show the popup again
+          this.popup && this.popup.remove();
+          this.popup = new this.Popup();
+
+          const props = e.features[0].properties;
+          this.popup.setLngLat(e.lngLat)
+            .setDOMContent(
+              render(
+                Popup({
+                  title: props.fmu_name,
+                  list: [{
+                    label: 'Company',
+                    value: props.company_na
+                  }, {
+                    label: 'CCF status',
+                    value: props.ccf_status
+                  }, {
+                    label: 'Type',
+                    value: props.fmu_type
+                  }]
+                }),
+                window.document.createElement('div')
+              )
+            )
+            .addTo(this.map);
+        },
+        mouseenter() {
+          this.map.getCanvas().style.cursor = 'pointer';
+        },
+        mousemove(e) {
+          this.map.getCanvas().style.cursor = 'pointer';
+          this.map.setFilter('forest_concession_layer_hover', ['==', 'cartodb_id', e.features[0].properties.cartodb_id]);
+        },
+        mouseleave() {
+          this.map.getCanvas().style.cursor = '';
+          this.map.setFilter('forest_concession_layer_hover', ['==', 'cartodb_id', '']);
+        }
+      }
+    }]
+  },
+
+
+  {
+    id: 'harvestable_areas',
+    provider: 'cartodb',
+    source: {
+      type: 'geojson',
+      data: `https://simbiotica.carto.com/api/v2/sql?q=${encodeURIComponent('SELECT * FROM harvestable_areas')}&format=geojson`
+    },
+    layers: [{
+      id: 'harvestable_areas_layer',
+      type: 'fill',
+      source: 'harvestable_areas',
+      layout: {},
+      paint: {
+        'fill-color': '#005b23',
+        'fill-opacity': 0.8,
+        'fill-outline-color': '#004219'
+      },
+      interactivity: {
+        click(e) {
+          // Remove always the popup if exists and you are using 'closeOnClick'
+          this.popup && this.popup.remove();
+          this.popup = new this.Popup();
+
+          const props = e.features[0].properties;
+
+          this.popup.setLngLat(e.lngLat)
+            .setDOMContent(
+              render(
+                Popup({
+                  title: props.num_ccf,
+                  list: []
+                }),
+                window.document.createElement('div')
+              )
+            )
+            .addTo(this.map);
+        },
+        mouseenter() {
+          this.map.getCanvas().style.cursor = 'pointer';
+          this.map.setFilter('harvestable_areas_layer_hover', ['==', 'cartodb_id', '']);
+        },
+        mousemove(e) {
+          this.map.getCanvas().style.cursor = 'pointer';
+          this.map.setFilter('harvestable_areas_layer_hover', ['==', 'cartodb_id', e.features[0].properties.cartodb_id]);
+        },
+        mouseleave() {
+          this.map.getCanvas().style.cursor = '';
+          this.map.setFilter('harvestable_areas_layer_hover', ['==', 'cartodb_id', '']);
+        }
+      }
+    }, {
+      id: 'harvestable_areas_layer_hover',
+      type: 'fill',
+      source: 'harvestable_areas',
+      layout: {},
+      paint: {
+        'fill-color': '#004219'
+      },
+      filter: ['==', 'cartodb_id', '']
+    }]
+  }
+];
+
+
 // TODO: replace this const to an API call
 const DOCUMENTATION_OPERATORS_DETAIL = [{
   id: 1,
@@ -219,155 +364,129 @@ const OBSERVATIONS_OPERATORS_DETAIL = [{
   id: 19,
   title: 'Sample observations title lorem',
   year: 2014,
+  severity: 1,
+  category: 'Forest Management',
+  illegality: 'Harvesting without concession title'
+}, {
+  id: 20,
+  title: 'Sample observations title lorem ipsum title lorem ipsum title lorem ipsum',
+  year: 2017,
+  severity: 1,
+  category: 'Use rights',
+  illegality: 'Harvesting without concession title'
+}, {
+  id: 21,
+  title: 'Sample observations title lorem',
+  year: 2017,
+  severity: 3,
+  category: 'Use rights',
+  illegality: 'Harvesting without concession title'
+}, {
+  id: 22,
+  title: 'Sample observations title lorem ipsum',
+  year: 2017,
+  severity: 3,
+  category: 'Legal registration',
+  illegality: 'Harvesting without concession title'
+}, {
+  id: 23,
+  title: 'Sample observations title lorem ipsum title lorem ipsum title lorem ipsum',
+  year: 2016,
+  severity: 3,
+  category: 'Legal registration',
+  illegality: 'Harvesting without concession title'
+}, {
+  id: 24,
+  title: 'Sample observations title lorem',
+  year: 2016,
+  severity: 2,
+  category: 'Harvesting',
+  illegality: 'Harvesting without concession title'
+}, {
+  id: 25,
+  title: 'Sample observations title lorem',
+  year: 2015,
+  severity: 0,
+  category: 'Harvesting',
+  illegality: 'Harvesting without concession title'
+}, {
+  id: 26,
+  title: 'Sample observations title lorem',
+  year: 2015,
+  severity: 0,
+  category: 'Forest Management',
+  illegality: 'Harvesting without concession title'
+}, {
+  id: 27,
+  title: 'Sample observations title lorem',
+  year: 2015,
+  severity: 3,
+  category: 'Forest Management',
+  illegality: 'Harvesting without concession title'
+}, {
+  id: 28,
+  title: 'Sample observations title lorem',
+  year: 2017,
+  severity: 3,
+  category: 'Forest Management',
+  illegality: 'Harvesting without concession title'
+}, {
+  id: 29,
+  title: 'Sample observations title lorem',
+  year: 2013,
+  severity: 3,
+  category: 'Forest Management',
+  illegality: 'Harvesting without concession title'
+}, {
+  id: 30,
+  title: 'Sample observations title lorem',
+  year: 2016,
+  severity: 1,
+  category: 'Forest Management',
+  illegality: 'Harvesting without concession title'
+}, {
+  id: 31,
+  title: 'Sample observations title lorem',
+  year: 2017,
+  severity: 1,
+  category: 'Forest Management',
+  illegality: 'Harvesting without concession title'
+}, {
+  id: 32,
+  title: 'Sample observations title lorem',
+  year: 2016,
+  severity: 0,
+  category: 'Forest Management',
+  illegality: 'Harvesting without concession title'
+}, {
+  id: 33,
+  title: 'Sample observations title lorem',
+  year: 2013,
+  severity: 0,
+  category: 'Forest Management',
+  illegality: 'Harvesting without concession title'
+}, {
+  id: 34,
+  title: 'Sample observations title lorem',
+  year: 2013,
+  severity: 0,
+  category: 'Forest Management',
+  illegality: 'Harvesting without concession title'
+}, {
+  id: 35,
+  title: 'Sample observations title lorem',
+  year: 2013,
+  severity: 0,
+  category: 'Forest Management',
+  illegality: 'Harvesting without concession title'
+}, {
+  id: 36,
+  title: 'Sample observations title lorem',
+  year: 2014,
   severity: 0,
   category: 'Forest Management',
   illegality: 'Harvesting without concession title'
 }];
-
-
-const MAP_OPTIONS_OPERATORS_DETAIL = {
-  zoom: 5,
-  center: [18, 0],
-  scrollZoom: false
-};
-
-const MAP_LAYERS_OPERATORS_DETAIL = [
-  {
-    id: 'forest_concession',
-    provider: 'cartodb',
-    source: {
-      type: 'geojson',
-      data: `https://simbiotica.carto.com/api/v2/sql?q=${encodeURIComponent('SELECT * FROM forest_concession')}&format=geojson`
-    },
-    layers: [{
-      id: 'forest_concession_layer_hover',
-      type: 'fill',
-      source: 'forest_concession',
-      layout: {},
-      paint: {
-        'fill-color': '#d07500',
-        'fill-opacity': 0.8,
-        'fill-outline-color': '#d07500'
-      },
-      filter: ['==', 'cartodb_id', '']
-    }, {
-      id: 'forest_concession_layer',
-      type: 'fill',
-      source: 'forest_concession',
-      layout: {},
-      paint: {
-        'fill-color': '#e98300',
-        'fill-opacity': 0.8,
-        'fill-outline-color': '#d07500'
-      },
-      interactivity: {
-        click(e) {
-          // Remove always the popup if exists and you are using 'closeOnClick'
-          // You will prevent a bug that doesn't show the popup again
-          this.popup && this.popup.remove();
-          this.popup = new this.Popup();
-
-          const props = e.features[0].properties;
-          this.popup.setLngLat(e.lngLat)
-            .setDOMContent(
-              render(
-                Popup({
-                  title: props.fmu_name,
-                  list: [{
-                    label: 'Company',
-                    value: props.company_na
-                  }, {
-                    label: 'CCF status',
-                    value: props.ccf_status
-                  }, {
-                    label: 'Type',
-                    value: props.fmu_type
-                  }]
-                }),
-                window.document.createElement('div')
-              )
-            )
-            .addTo(this.map);
-        },
-        mouseenter() {
-          this.map.getCanvas().style.cursor = 'pointer';
-        },
-        mousemove(e) {
-          this.map.getCanvas().style.cursor = 'pointer';
-          this.map.setFilter('forest_concession_layer_hover', ['==', 'cartodb_id', e.features[0].properties.cartodb_id]);
-        },
-        mouseleave() {
-          this.map.getCanvas().style.cursor = '';
-          this.map.setFilter('forest_concession_layer_hover', ['==', 'cartodb_id', '']);
-        }
-      }
-    }]
-  },
-
-
-  {
-    id: 'harvestable_areas',
-    provider: 'cartodb',
-    source: {
-      type: 'geojson',
-      data: `https://simbiotica.carto.com/api/v2/sql?q=${encodeURIComponent('SELECT * FROM harvestable_areas')}&format=geojson`
-    },
-    layers: [{
-      id: 'harvestable_areas_layer',
-      type: 'fill',
-      source: 'harvestable_areas',
-      layout: {},
-      paint: {
-        'fill-color': '#005b23',
-        'fill-opacity': 0.8,
-        'fill-outline-color': '#004219'
-      },
-      interactivity: {
-        click(e) {
-          // Remove always the popup if exists and you are using 'closeOnClick'
-          this.popup && this.popup.remove();
-          this.popup = new this.Popup();
-
-          const props = e.features[0].properties;
-
-          this.popup.setLngLat(e.lngLat)
-            .setDOMContent(
-              render(
-                Popup({
-                  title: props.num_ccf,
-                  list: []
-                }),
-                window.document.createElement('div')
-              )
-            )
-            .addTo(this.map);
-        },
-        mouseenter() {
-          this.map.getCanvas().style.cursor = 'pointer';
-          this.map.setFilter('harvestable_areas_layer_hover', ['==', 'cartodb_id', '']);
-        },
-        mousemove(e) {
-          this.map.getCanvas().style.cursor = 'pointer';
-          this.map.setFilter('harvestable_areas_layer_hover', ['==', 'cartodb_id', e.features[0].properties.cartodb_id]);
-        },
-        mouseleave() {
-          this.map.getCanvas().style.cursor = '';
-          this.map.setFilter('harvestable_areas_layer_hover', ['==', 'cartodb_id', '']);
-        }
-      }
-    }, {
-      id: 'harvestable_areas_layer_hover',
-      type: 'fill',
-      source: 'harvestable_areas',
-      layout: {},
-      paint: {
-        'fill-color': '#004219'
-      },
-      filter: ['==', 'cartodb_id', '']
-    }]
-  }
-];
-
 
 export {
   TABS_OPERATORS_DETAIL,
