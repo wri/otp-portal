e004fce185736ead51fdb502c0ca5a68ddfbb9f8
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-06-12 10:30:38 +0200

    Footer: little amendment

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -5,32 +5,36 @@ import NavigationList from 'components/ui/navigation-list';
 
 export default() => (
   <footer className="c-footer">
-    <div className="l-container upper-footer">
-      <div className="footer-container">
-        <div className="footer-item">
-          <h1 className="logo">
-            <Link prefetch href="/">
-              <a>Open Timber Portal</a>
-            </Link>
-          </h1>
-        </div>
-        <div className="footer-item">
-          <NavigationList hideActive />
+    <div className="upper-footer">
+      <div className="l-container">
+        <div className="footer-container">
+          <div className="footer-item">
+            <h1 className="logo">
+              <Link prefetch href="/">
+                <a>Open Timber Portal</a>
+              </Link>
+            </h1>
+          </div>
+          <div className="footer-item">
+            <NavigationList hideActive />
+          </div>
         </div>
       </div>
     </div>
 
-    <div className="l-container lower-footer">
-      <div className="footer-container">
-        <div className="footer-item">
-          <a href="http://www.wri.org/" target="_blank" rel="noreferrer noopener">
-            <img src="/static/images/logos/wri-logo.svg" alt="WRI logo" />
-          </a>
-        </div>
-        <div className="footer-item">
-          <p className="-bold">World Resources Institute</p>
-          <p>10 G Street NE Suite 800, Washington, DC 20002, USA</p>
-          <p>Phone +1(202) 729-7600    |    Fax: +1 (202) 720 7610</p>
+    <div className="lower-footer">
+      <div className="l-container">
+        <div className="footer-container">
+          <div className="footer-item">
+            <a href="http://www.wri.org/" target="_blank" rel="noreferrer noopener">
+              <img src="/static/images/logos/wri-logo.svg" alt="WRI logo" />
+            </a>
+          </div>
+          <div className="footer-item">
+            <p className="-bold">World Resources Institute</p>
+            <p>10 G Street NE Suite 800, Washington, DC 20002, USA</p>
+            <p>Phone +1(202) 729-7600    |    Fax: +1 (202) 720 7610</p>
+          </div>
         </div>
       </div>
     </div>
