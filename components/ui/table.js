7bc83d82c8d0b7926f3177aab0b6319b61352a2f
bitTal <albafjez@gmail.com>
2017-06-14 10:58:37 +0200

    Update obs table with data

--- components/ui/table.js
+++ components/ui/table.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactTable from 'react-table';
+import isEqual from 'lodash/isEqual';
 import { TABLE_HEADERS } from 'constants/observations';
 
 
@@ -10,23 +11,26 @@ export default class StaticSection extends React.Component {
 
     return (
       <div className="c-table">
-        <ReactTable
-          data={data}
-          className="table"
-          columns={TABLE_HEADERS}
-          defaultPageSize={options.pageSize}
-          showPagination={options.pagination}
-          page={options.page}
-          previousText={options.previousText}
-          nextText={options.nextText}
-          noDataText={options.noDataText}
-          pages={options.pages}
-          showPageSizeOptions={options.showPageSizeOptions}
-          sortable
-          // Api pagination & sort
-          // manual={options.manual}
-          // onPageChange={options.onPageChange}
-        />
+        {data.length > 0 ?
+          <ReactTable
+            data={data}
+            className="table"
+            columns={TABLE_HEADERS}
+            defaultPageSize={options.pageSize}
+            showPagination={options.pagination}
+            page={options.page}
+            previousText={options.previousText}
+            nextText={options.nextText}
+            noDataText={options.noDataText}
+            pages={options.pages}
+            showPageSizeOptions={options.showPageSizeOptions}
+            sortable
+            // Api pagination & sort
+            // manual={options.manual}
+            // onPageChange={options.onPageChange}
+          /> :
+          <p></p>
+        }
       </div>
     );
   }
