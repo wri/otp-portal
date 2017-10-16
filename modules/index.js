01e14cac86ed736f95abeb93bf3e4dc5f8f4df0d
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-10-16 18:53:19 +0200

    Partners: grid

--- modules/index.js
+++ modules/index.js
@@ -3,6 +3,7 @@ export { default as modal } from './modal';
 export { default as observations } from './observations';
 export { default as operators } from './operators';
 export { default as operatorsDetail } from './operators-detail';
+export { default as partners } from './partners';
 export { default as user } from './user';
 
 // Toastr
