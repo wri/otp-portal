0a14e0bc7c63b440d3288cfa144618a46090d446
Ian Denty <ian.denty@vizzuality.com>
2018-01-26 18:23:13 +0100

    Observations: add interaction to toggle table columns

--- pages/observations.js
+++ pages/observations.js
@@ -27,6 +27,7 @@ import Spinner from 'components/ui/spinner';
 import StaticTabs from 'components/ui/static-tabs';
 import { ReadMore } from 'react-read-more';
 import Icon from 'components/ui/icon';
+import CheckboxGroup from '../components/form/CheckboxGroup';
 
 // Utils
 import {
@@ -34,7 +35,8 @@ import {
   getFilters,
   setFilters,
   setObservationsUrl,
-  getObservationsUrl
+  getObservationsUrl,
+  setActiveColumns
 } from 'modules/observations';
 
 // Constants
@@ -102,6 +104,83 @@ class ObservationsPage extends Page {
   render() {
     const { url, observations, parsedChartObservations, parsedTableObservations } = this.props;
 
+    // Hard coded values
+    const inputs = ['date', 'country', 'operator', 'category', 'observation', 'level', 'fmu', 'report'];
+
+    const tableOptions = inputs
+      .map(column => ({
+        label: `${column.charAt(0).toUpperCase()}${column.slice(1)}`,
+        value: column
+      }));
+
+    const columnHeaders = [
+      {
+        Header: <span className="sortable">{this.props.intl.formatMessage({ id: 'date' })}</span>,
+        accessor: 'date',
+        minWidth: 75
+      },
+      {
+        Header: <span className="sortable">{this.props.intl.formatMessage({ id: 'country' })}</span>,
+        accessor: 'country',
+        className: '-uppercase',
+        minWidth: 100
+      },
+      {
+        Header: <span className="sortable">{this.props.intl.formatMessage({ id: 'operator' })}</span>,
+        accessor: 'operator',
+        className: '-uppercase description',
+        minWidth: 120
+      },
+      {
+        Header: <span className="sortable">{this.props.intl.formatMessage({ id: 'fmu' })}</span>,
+        accessor: 'fmu',
+        className: 'description',
+        minWidth: 120
+      },
+      {
+        Header: <span className="sortable">{this.props.intl.formatMessage({ id: 'category' })}</span>,
+        accessor: 'category',
+        headerClassName: '-a-left',
+        className: 'description',
+        minWidth: 120
+      },
+      {
+        Header: <span className="sortable">{this.props.intl.formatMessage({ id: 'detail' })}</span>,
+        accessor: 'observation',
+        headerClassName: '-a-left',
+        className: 'description',
+        minWidth: 250,
+        Cell: attr => (
+          <ReadMore lines={1} text="more">
+            {attr.value}
+          </ReadMore>
+        )
+      },
+      {
+        Header: <span className="sortable">{this.props.intl.formatMessage({ id: 'severity' })}</span>,
+        accessor: 'level',
+        headerClassName: 'severity-th',
+        className: 'severity',
+        Cell: attr => <span className={`severity-item -sev-${attr.value}`}>{attr.value}</span>
+      },
+      {
+        Header: <span className="sortable">{this.props.intl.formatMessage({ id: 'report' })}</span>,
+        accessor: 'report',
+        headerClassName: '',
+        className: 'report',
+        Cell: attr =>
+          <div className="report-item-wrapper">
+            { attr.value ?
+              <a href={attr.value} className="report-item">
+                <Icon className="" name="icon-file-empty" />
+              </a>
+            :
+              <span className="report-item-text">-</span>
+          }
+          </div>
+      }
+    ];
+
     return (
       <Layout
         title="Observations"
@@ -147,79 +226,22 @@ class ObservationsPage extends Page {
           <div className="c-section -relative">
             <div className="l-container">
               <Spinner isLoading={observations.loading} className="" />
+              <div className="c-field -fluid -valid">
+                <CheckboxGroup
+                  className="-inline -small -single-row"
+                  name="observations-columns"
+                  onChange={value => this.props.setActiveColumns(value)}
+                  properties={{ default: observations.columns, name: 'observations-columns' }}
+                  options={tableOptions}
+                />
+              </div>
 
               {this.state.tab === 'observations-list' &&
                 <Table
                   sortable
                   data={parsedTableObservations}
                   options={{
-                    columns: [
-                      {
-                        Header: <span className="sortable">{this.props.intl.formatMessage({ id: 'date' })}</span>,
-                        accessor: 'date',
-                        minWidth: 75
-                      },
-                      {
-                        Header: <span className="sortable">{this.props.intl.formatMessage({ id: 'country' })}</span>,
-                        accessor: 'country',
-                        className: '-uppercase',
-                        minWidth: 100
-                      },
-                      {
-                        Header: <span className="sortable">{this.props.intl.formatMessage({ id: 'operator' })}</span>,
-                        accessor: 'operator',
-                        className: '-uppercase description',
-                        minWidth: 120
-                      },
-                      {
-                        Header: <span className="sortable">{this.props.intl.formatMessage({ id: 'fmu' })}</span>,
-                        accessor: 'fmu',
-                        className: 'description',
-                        minWidth: 120
-                      },
-                      {
-                        Header: <span className="sortable">{this.props.intl.formatMessage({ id: 'category' })}</span>,
-                        accessor: 'category',
-                        headerClassName: '-a-left',
-                        className: 'description',
-                        minWidth: 120
-                      },
-                      {
-                        Header: <span className="sortable">{this.props.intl.formatMessage({ id: 'detail' })}</span>,
-                        accessor: 'observation',
-                        headerClassName: '-a-left',
-                        className: 'description',
-                        minWidth: 250,
-                        Cell: attr => (
-                          <ReadMore lines={1} text="more">
-                            {attr.value}
-                          </ReadMore>
-                        )
-                      },
-                      {
-                        Header: <span className="sortable">{this.props.intl.formatMessage({ id: 'severity' })}</span>,
-                        accessor: 'level',
-                        headerClassName: 'severity-th',
-                        className: 'severity',
-                        Cell: attr => <span className={`severity-item -sev-${attr.value}`}>{attr.value}</span>
-                      },
-                      {
-                        Header: <span className="sortable">{this.props.intl.formatMessage({ id: 'report' })}</span>,
-                        accessor: 'report',
-                        headerClassName: '',
-                        className: 'report',
-                        Cell: attr =>
-                          <div className="report-item-wrapper">
-                            { attr.value ?
-                              <a href={attr.value} className="report-item">
-                                <Icon className="" name="icon-file-empty" />
-                              </a>
-                            :
-                              <span className="report-item-text">-</span>
-                          }
-                          </div>
-                      }
-                    ],
+                    columns: columnHeaders.filter(header => observations.columns.includes(header.accessor)),
                     pageSize: this.getPageSize(),
                     pagination: true,
                     previousText: '<',
@@ -280,6 +302,9 @@ export default withIntl(withRedux(
     setFilters(filter) {
       dispatch(setFilters(filter));
       dispatch(setObservationsUrl());
+    },
+    setActiveColumns(activeColumns) {
+      dispatch(setActiveColumns(activeColumns));
     }
   })
 )(ObservationsPage));
