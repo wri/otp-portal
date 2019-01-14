19f7a45bd2bb319795b05b0252c4f4afc8f4f06d
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2019-01-14 10:35:46 +0100

    Language: zh-CN prop is send to the API

--- modules/operators-detail.js
+++ modules/operators-detail.js
@@ -139,9 +139,11 @@ export function getOperator(id) {
       'operator-document-fmus.fmu'
     ];
 
+    const language = Cookies.get('language');
+
     const queryParams = queryString.stringify({
       include: includeFields.join(','),
-      locale: Cookies.get('language')
+      locale: language === 'zh' ? 'zh-CN' : language // This is not very good...
     });
 
 
