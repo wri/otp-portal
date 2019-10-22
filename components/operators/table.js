d4be089d1437b260879002028159cdcc02fbf735
Miguel Barrenechea Sánchez <miguelbarrenechea@MacBook-Pro.home>
2019-10-22 17:33:22 +0200

    Map: europe bug

---
+++ components/operators/table.js
@@ -0,0 +1,184 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import sortBy from 'lodash/sortBy';
+
+// Next
+import Link from 'next/link';
+
+// Intl
+import { injectIntl, intlShape } from 'react-intl';
+
+// Utils
+import { HELPERS_DOC } from 'utils/documentation';
+
+import Spinner from 'components/ui/spinner';
+import Icon from 'components/ui/icon';
+
+// Chart
+import OperatorsRanking from 'components/operators/ranking';
+import OperatorsCertificationsTd from 'components/operators/certificationsTd';
+
+class OperatorsTable extends React.Component {
+  state = {
+    sortColumn: 'documentation',
+    sortDirection: -1,
+    table: [],
+    max: null
+  }
+
+  componentDidMount() {
+    const { operators } = this.props;
+
+    if (operators.length) {
+      this.setState({
+        sortColumn: this.state.sortColumn || 'documentation',
+        sortDirection: this.state.sortDirection || -1,
+        table: operators.map(o => ({
+          id: o.id,
+          name: o.name,
+          certification: <OperatorsCertificationsTd fmus={o.fmus} />,
+          score: o.score || 0,
+          obs_per_visit: o['obs-per-visit'] || 0,
+          documentation: HELPERS_DOC.getPercentage(o),
+          fmus: o.fmus ? o.fmus.length : 0
+        })),
+        max: Math.max(...operators.map(o => o.observations.length))
+      });
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.operators.length !== this.props.operators.length) {
+      this.setState({
+        sortColumn: this.state.sortColumn || 'documentation',
+        sortDirection: this.state.sortDirection || -1,
+        table: nextProps.operators.map(o => ({
+          id: o.id,
+          name: o.name,
+          certification: <OperatorsCertificationsTd fmus={o.fmus} />,
+          score: o.score || 0,
+          obs_per_visit: o['obs-per-visit'] || 0,
+          documentation: HELPERS_DOC.getPercentage(o),
+          fmus: o.fmus ? o.fmus.length : 0
+        })),
+        max: Math.max(...nextProps.operators.map(o => o.observations.length))
+      });
+    }
+  }
+
+  sortBy = (column) => {
+    this.setState({
+      sortColumn: column,
+      sortDirection: this.state.sortDirection * -1
+    });
+  };
+
+  render() {
+    const { operators } = this.props;
+    const { sortColumn, sortDirection, table } = this.state;
+
+    if (!operators.loading) {
+      return (
+        <div className="c-ranking">
+          <table>
+            <thead>
+              <tr>
+                <th />
+                <th
+                  className="td-documentation -ta-center -sort"
+                  onClick={() => {
+                    this.sortBy("documentation");
+                  }}
+                >
+                  {this.props.intl.formatMessage({
+                    id: "operators.table.upload_docs"
+                  })}
+                  {sortDirection === -1 && (
+                    <Icon name="icon-arrow-down" className="-tiny" />
+                  )}
+                  {sortDirection === 1 && (
+                    <Icon name="icon-arrow-up" className="-tiny" />
+                  )}
+                </th>
+                <th className="-ta-left">
+                  {this.props.intl.formatMessage({
+                    id: "operators.table.name"
+                  })}
+                </th>
+
+                {/* Other styles */}
+                <th className="-ta-center -break-ponit -contextual">
+                  {this.props.intl.formatMessage({
+                    id: "operators.table.obs_visit"
+                  })}
+                </th>
+                <th className="-contextual">
+                  {this.props.intl.formatMessage({
+                    id: "operators.table.fmus"
+                  })}
+                </th>
+                <th className="-contextual">
+                  {this.props.intl.formatMessage({
+                    id: "operators.table.certification"
+                  })}
+                </th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {sortBy(table, o => sortDirection * o[sortColumn]).map((r, i) => (
+                <tr key={`${r.id}-ranking`}>
+                  {i === 0 && (
+                    <td className="-ta-center" rowSpan={table.length}>
+                      <OperatorsRanking
+                        key={`update-${r.id}`}
+                        data={table.map(o => ({
+                          id: o.id,
+                          value: parseInt(o.documentation, 10)
+                        }))}
+                        sortDirection={sortDirection}
+                      />
+                    </td>
+                  )}
+                  <td
+                    id={`td-documentation-${r.id}`}
+                    className="td-documentation -ta-left"
+                  >
+                    {r.documentation}%
+                  </td>
+                  <td className="-ta-left">
+                    <Link
+                      href={{
+                        pathname: "/operators-detail",
+                        query: { id: r.id }
+                      }}
+                      as={`/operators/${r.id}`}
+                    >
+                      <a>{r.name}</a>
+                    </Link>
+                  </td>
+                  <td className="-ta-center">
+                    {!!r.obs_per_visit && r.obs_per_visit}
+                    {!r.obs_per_visit && (
+                      <div className="stoplight-dot -state-0}" />
+                    )}
+                  </td>
+                  <td className="-ta-right"> {r.fmus} </td>
+                  <td className="-ta-right">{r.certification}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    }
+    return <Spinner isLoading />;
+  }
+}
+
+OperatorsTable.propTypes = {
+  operators: PropTypes.array.isRequired,
+  intl: intlShape.isRequired
+};
+
+export default injectIntl(OperatorsTable);
--- pages/operators.js
+++ pages/operators.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import debounce from 'lodash/debounce';
-import sortBy from 'lodash/sortBy';
 import flatten from 'lodash/flatten';
 
 import * as Cookies from 'js-cookie';
-
-// Next
-import Link from 'next/link';
 
 // Toastr
 import { toastr } from 'react-redux-toastr';
@@ -14,9 +10,6 @@ import { toastr } from 'react-redux-toastr';
 // Intl
 import withIntl from 'hoc/with-intl';
 import { intlShape } from 'react-intl';
-
-// Utils
-import { HELPERS_DOC } from 'utils/documentation';
 
 // Redux
 import withRedux from 'next-redux-wrapper';
@@ -32,46 +25,17 @@ import { MAP_LAYERS_OPERATORS } from 'constants/operators';
 import Page from 'components/layout/page';
 import Layout from 'components/layout/layout';
 import Sidebar from 'components/ui/sidebar';
-import Spinner from 'components/ui/spinner';
-import Icon from 'components/ui/icon';
 import Map from 'components/map/map';
 import MapLegend from 'components/map/map-legend';
 import MapControls from 'components/map/map-controls';
 import ZoomControl from 'components/map/controls/zoom-control';
 
 // Operators components
-import OperatorsRanking from 'components/operators/ranking';
 import OperatorsFilters from 'components/operators/filters';
-import OperatorsCertificationsTd from 'components/operators/certificationsTd';
+import OperatorsTable from 'components/operators/table';
 
 
 class OperatorsPage extends Page {
-
-  static parseData(operators = []) {
-    return {
-      sortColumn: 'documentation',
-      sortDirection: -1,
-      table: operators.map(o => ({
-        id: o.id,
-        name: o.name,
-        certification: <OperatorsCertificationsTd fmus={o.fmus} />,
-        score: o.score || 0,
-        obs_per_visit: o['obs-per-visit'] || 0,
-        documentation: HELPERS_DOC.getPercentage(o),
-        fmus: (o.fmus) ? o.fmus.length : 0
-      })),
-      max: Math.max(...operators.map(o => o.observations.length))
-    };
-  }
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      ...OperatorsPage.parseData(props.operatorsRanking.data)
-    };
-  }
-
   /* Component Lifecycle */
   componentDidMount() {
     const { url, operators, operatorsRanking } = this.props;
@@ -107,25 +71,8 @@ class OperatorsPage extends Page {
     }
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.operatorsRanking.data.length !== this.props.operatorsRanking.data.length) {
-      this.setState(OperatorsPage.parseData(nextProps.operatorsRanking.data));
-    }
-  }
-
   componentWillUnMount() {
     toastr.remove('operators.disclaimer');
-  }
-
-  /**
-   * UI EVENTS
-   * - sortBy
-  */
-  sortBy = (column) => {
-    this.setState({
-      sortColumn: column,
-      sortDirection: this.state.sortDirection * -1
-    });
   }
 
   toggleLayers = (id, checked) => {
@@ -135,93 +82,6 @@ class OperatorsPage extends Page {
       [...activeLayers, id] : activeLayers.filter(layerId => layerId !== id);
 
     this.props.setActiveMapLayers(toggledLayers);
-  }
-
-  /**
-   * HELPERS
-   * - getOperatorsTable
-   * - getOperatorsRanking
-  */
-  getOperatorsTable() {
-    const operatorsRanking = this.props.operatorsRanking;
-    const { sortColumn, sortDirection, table } = this.state;
-
-    if (!operatorsRanking.loading) {
-      return (
-        <div className="c-ranking">
-          <table>
-            <thead>
-              <tr>
-                <th />
-                <th
-                  className="td-documentation -ta-center -sort"
-                  onClick={() => { this.sortBy('documentation'); }}
-                >
-                  {this.props.intl.formatMessage({ id: 'operators.table.upload_docs' })}
-                  {sortDirection === -1 && <Icon name="icon-arrow-down" className="-tiny" />}
-                  {sortDirection === 1 && <Icon name="icon-arrow-up" className="-tiny" />}
-                </th>
-                <th className="-ta-left">
-                  {this.props.intl.formatMessage({ id: 'operators.table.name' })}
-                </th>
-
-                {/* Other styles */}
-                <th className="-ta-center -break-ponit -contextual">
-                  {this.props.intl.formatMessage({ id: 'operators.table.obs_visit' })}
-                </th>
-                <th className="-contextual">
-                  {this.props.intl.formatMessage({ id: 'operators.table.fmus' })}
-                </th>
-                <th className="-contextual">
-                  {this.props.intl.formatMessage({ id: 'operators.table.certification' })}
-                </th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {sortBy(table, o => sortDirection * o[sortColumn]).map((r, i) => (
-                <tr key={r.id}>
-                  {i === 0 &&
-                  <td className="-ta-center" rowSpan={table.length}>
-                    <OperatorsRanking
-                      key={`update-${r.id}`}
-                      data={table.map(o => ({ id: o.id, value: parseInt(o.documentation, 10) }))}
-                      sortDirection={sortDirection}
-                    />
-                  </td>
-                    }
-                  <td
-                    id={`td-documentation-${r.id}`}
-                    className="td-documentation -ta-left"
-                  >
-                    {r.documentation}%
-                    </td>
-                  <td className="-ta-left">
-                    <Link
-                      href={{ pathname: '/operators-detail', query: { id: r.id } }}
-                      as={`/operators/${r.id}`}
-                    >
-                      <a>{r.name}</a>
-                    </Link>
-                  </td>
-                  <td className="-ta-center">
-                    {!!r.obs_per_visit && r.obs_per_visit}
-                    {!r.obs_per_visit &&
-                    <div className="stoplight-dot -state-0}" />
-                      }
-                  </td>
-                  <td className="-ta-right"> {r.fmus} </td>
-                  <td className="-ta-right">
-                    {r.certification}
-                  </td>
-                </tr>
-                ))}
-            </tbody>
-          </table>
-        </div>
-      );
-    }
-    return <Spinner isLoading />;
   }
 
   render() {
@@ -247,10 +107,10 @@ class OperatorsPage extends Page {
         <div className="c-section -map">
           <Sidebar>
             <OperatorsFilters />
-            {this.getOperatorsTable()}
+            <OperatorsTable operators={operatorsRanking.data} />
           </Sidebar>
 
-          <div className="c-map-container">
+          <div className="c-map-container -absolute" style={{ width: 'calc(100% - 650px)', left: 650 }}>
             {/* Map */}
             <Map
               mapFilters={{
