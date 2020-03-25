d92f3d13730d772abb1f7356c64e380b1f83601a
Miguel Barrenechea Sánchez <miguelbarrenechea@MacBook-Pro.home>
2020-03-25 17:40:11 +0100

    FA Attributions

---
+++ components/map-new/fa-attributions/attributions.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    name: 'Democratic Republic of Congo',
+    licenses: [
+      {
+        name: 'Forest conncession agreement',
+        data: 'http://cod-data.forest-atlas.org/datasets/forest-conncession-agreement/data',
+        copyright: '© 2017 MECNDD, World Resources Institute'
+      }
+    ]
+  },
+  {
+    name: 'Cameroon',
+    licenses: [
+      {
+        name: 'Concessions forestieres',
+        data: 'http://cmr-data.forest-atlas.org/datasets/concessions-forestieres/data',
+        copyright: '© 2017 MINFOF, World Resources Institute'
+      },
+      {
+        name: 'Sociétes forestieres',
+        data: 'http://cmr-data.forest-atlas.org/datasets/soci%C3%A9tes-forestieres/data',
+        copyright: '© 2017 MINFOF, World Resources Institute'
+      },
+      {
+        name: 'Ventes de coupe',
+        data: 'http://cmr-data.forest-atlas.org/datasets/ventes-de-coupe/data',
+        copyright: '© 2017 MINFOF, World Resources Institute'
+      }
+    ]
+  },
+  {
+    name: 'Congo',
+    licenses: [
+      {
+        name: 'Concessions Forestières',
+        data: 'http://cog-data.forest-atlas.org/datasets/concessions-foresti%C3%A8res-/data',
+        copyright: '© 2017 MEFDD, World Resources Institute'
+      }
+    ]
+  },
+  {
+    name: 'Gabon',
+    licenses: [
+      {
+        name: 'Concessions forestières',
+        data: 'http://gab-data.forest-atlas.org/datasets/concessions-foresti%C3%A8res/data',
+        copyright: '© 2017 Your Organization'
+      }
+    ]
+  },
+  {
+    name: 'Central African Republic',
+    licenses: [
+      {
+        name: "Permis d'Exploitation et d'Aménagement",
+        data: 'http://data-mefcp.opendata.arcgis.com/datasets/permis-dexploitation-et-dam%C3%A9nagement/data',
+        copyright: '© 2017 Your Organization'
+      }
+    ]
+  }
+];
--- components/map-new/fa-attributions/component.js
+++ components/map-new/fa-attributions/component.js
@@ -1,56 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const ARRAY = [
-  {
-    iso3: 'CMR',
-    name: 'Cameroon',
-    urls: [
-      'http://cmr-data.forest-atlas.org/datasets/concessions-forestieres/data',
-      'http://cmr-data.forest-atlas.org/datasets/soci%C3%A9tes-forestieres/data',
-      'http://cmr-data.forest-atlas.org/datasets/ventes-de-coupe/data',
-      'http://cmr-data.forest-atlas.org/datasets/soci%C3%A9tes-forestieres/data'
-    ]
-  },
-  {
-    iso3: 'CAF',
-    name: 'Central African Republic',
-    urls: [
-      'http://data-mefcp.opendata.arcgis.com/datasets/permis-dexploitation-et-dam%C3%A9nagement/data'
-    ]
-  },
-  {
-    iso3: 'COG',
-    name: 'Congo',
-    urls: [
-      'http://cog-data.forest-atlas.org/datasets/concessions-foresti%C3%A8res-/data'
-    ]
-  },
-  {
-    iso3: 'COD',
-    name: 'Democratic republic of congo',
-    urls: [
-      'http://cod-data.forest-atlas.org/datasets/forest-conncession-agreement/data'
-    ]
-  },
-  {
-    iso3: 'GAB',
-    name: 'Gabon',
-    urls: [
-      'http://gab-data.forest-atlas.org/datasets/concessions-foresti%C3%A8res/data?geometry=5.87%2C-0.608%2C16.323%2C1.315'
-    ]
-  }
-];
+import ATTRIBUTIONS from './attributions';
 
 export default function FAAttributions() {
   return (
     <div className="c-fa-attributions">
-      {ARRAY.map(attr => (
+      {ATTRIBUTIONS.map(attr => (
         <div className="attributions--item">
           <h3>{attr.name}</h3>
           <ul>
-            {attr.urls.map(u => (
-              <li><a target="_blank" rel="noopener noreferrer" href={u}>{u}</a></li>
+            {attr.licenses.map(u => (
+              <li>
+                <a target="_blank" rel="noopener noreferrer" href={u.data}><h4>{u.name}</h4></a>
+                <div className="attributions--copyright">{u.copyright}</div>
+              </li>
             ))}
           </ul>
         </div>
