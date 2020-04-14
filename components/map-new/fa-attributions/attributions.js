77908ea531585d6ce0b657bc68f57b22ad2b91a9
Miguel Barrenechea Sánchez <miguelbarrenechea@MacBook-Pro.home>
2020-04-14 18:10:12 +0200

    FA Attributions update

--- components/map-new/fa-attributions/attributions.js
+++ components/map-new/fa-attributions/attributions.js
@@ -1,5 +1,26 @@
 export default [
   {
+    name: 'Global',
+    licenses: [
+      {
+        name: 'GLAD alerts',
+        data: 'http://glad-forest-alert.appspot.com/',
+        copyright: 'Hansen, Matthew C., Alexander Krylov, Alexandra Tyukavina, Peter V. Potapov, Svetlana Turubanova, Bryan Zutta, Suspense Ifo, Belinda Margono, Fred Stolle, and Rebecca Moore. “Humid Tropical Forest Disturbance Alerts Using Landsat Data.” Environmental Research Letters 11, no. 3 (2016): 034008. https://doi.org/10.1088/1748-9326/11/3/034008.'
+      },
+      {
+        name: 'Tree cover loss',
+        data: 'http://earthenginepartners.appspot.com/science-2013-global-forest/download_v1.5.html',
+        copyright: 'Hansen/UMD/Google/USGS/NASA'
+      },
+      {
+        name: 'Tree cover gain',
+        data: 'http://earthenginepartners.appspot.com/science-2013-global-forest/download_v1.6.html',
+        copyright: 'Hansen/UMD/Google/USGS/NASA'
+      }
+
+    ]
+  },
+  {
     name: 'Democratic Republic of Congo',
     licenses: [
       {
@@ -26,6 +47,11 @@ export default [
         name: 'Ventes de coupe',
         data: 'http://cmr-data.forest-atlas.org/datasets/ventes-de-coupe',
         copyright: '© 2017 MINFOF, World Resources Institute'
+      },
+      {
+        name: 'Forêts de production',
+        data: 'http://cmr-data.forest-atlas.org/datasets/for%C3%AAts-de-production',
+        copyright: '© 2017 MINFOF, World Resources Institute'
       }
     ]
   },
@@ -45,7 +71,7 @@ export default [
       {
         name: 'Concessions forestières',
         data: 'http://gab-data.forest-atlas.org/datasets/concessions-foresti%C3%A8res',
-        copyright: '© 2017 Your Organization'
+        copyright: '© 2017 MPERNFM, World Resources Institute'
       }
     ]
   },
@@ -55,7 +81,7 @@ export default [
       {
         name: "Permis d'Exploitation et d'Aménagement",
         data: 'http://data-mefcp.opendata.arcgis.com/datasets/permis-dexploitation-et-dam%C3%A9nagement',
-        copyright: '© 2017 Your Organization'
+        copyright: '© 2017 MEFCP, World Resources Institute'
       }
     ]
   }
