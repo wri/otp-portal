e965c62e1ecbeb204e1e8ba3fe2e6d1a280e5cfe
Miguel Barrenechea Sánchez <miguelbarrenechea@MacBook-Pro.home>
2020-05-19 12:30:06 +0200

    Redirect countries link

--- index.js
+++ index.js
@@ -58,19 +58,22 @@ app.prepare()
   .then(() => {
     // COUNTRIES
     server.get('/countries', (req, res) => {
-      const { query } = parse(req.url, true);
-      return app.render(req, res, '/countries', Object.assign(req.params, query));
+      res.redirect('/');
+      // const { query } = parse(req.url, true);
+      // return app.render(req, res, '/countries', Object.assign(req.params, query));
     });
 
 
     server.get('/countries/:id', (req, res) => {
-      const { query } = parse(req.url, true);
-      return app.render(req, res, '/countries-detail', Object.assign(req.params, query));
+      res.redirect('/');
+      // const { query } = parse(req.url, true);
+      // return app.render(req, res, '/countries-detail', Object.assign(req.params, query));
     });
 
     server.get('/countries/:id/:tab', (req, res) => {
-      const { query } = parse(req.url, true);
-      return app.render(req, res, '/countries-detail', Object.assign(req.params, query));
+      res.redirect('/');
+      // const { query } = parse(req.url, true);
+      // return app.render(req, res, '/countries-detail', Object.assign(req.params, query));
     });
 
     // OPERATORS
