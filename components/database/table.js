cd1245ae4d117a6f26e84fe34f2b3f79c9c9b5d2
Daniel Fernandez Rico <deferrico@gmail.com>
2020-12-14 17:55:00 +0100

    Fixed table headers

--- components/database/table.js
+++ components/database/table.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 // Components
-import Tooltip from 'rc-tooltip/dist/rc-tooltip';
 import CheckboxGroup from 'components/form/CheckboxGroup';
 import Table from 'components/ui/table';
 import Spinner from 'components/ui/spinner';
@@ -16,19 +15,14 @@ import withIntl from 'hoc/with-intl';
 import { intlShape } from 'react-intl';
 
 import { getParsedTableDocuments } from 'selectors/database/database';
-import { getObservations, setActiveColumns } from 'modules/observations';
-
-import { PALETTE_COLOR_1 } from 'constants/rechart';
+import { setActiveColumns } from 'modules/documents-database';
 
 function DatabaseTable({
-  observations,
+  database,
   parsedTableDocuments,
-  getObservations: _getObservations,
   setActiveColumns: _setActiveColumns,
   intl,
 }) {
-  const [page, setPage] = useState(1);
-  // Hard coded values
   const inputs = [
     'status',
     'country',
@@ -43,18 +37,6 @@ function DatabaseTable({
     // 'reason',
   ];
 
-  const changeOfLabelLookup = {
-    level: 'severity',
-    observation: 'detail',
-  };
-
-  const tableOptions = inputs.map((column) => ({
-    label: Object.keys(changeOfLabelLookup).includes(column)
-      ? intl.formatMessage({ id: changeOfLabelLookup[column] })
-      : intl.formatMessage({ id: column }),
-    value: column,
-  }));
-
   const columnHeaders = [
     {
       Header: (
@@ -62,7 +44,7 @@ function DatabaseTable({
           {intl.formatMessage({ id: 'documents' })}
         </span>
       ),
-      accessor: 'report',
+      accessor: 'document',
       headerClassName: '',
       className: 'report',
       Cell: (attr) => (
@@ -84,10 +66,21 @@ function DatabaseTable({
     },
     {
       Header: (
-        <span className="sortable">{intl.formatMessage({ id: 'date' })}</span>
+        <span className="sortable">
+          {intl.formatMessage({ id: 'doc.start_date' })}
+        </span>
       ),
-      accessor: 'date',
-      minWidth: 75,
+      accessor: 'start-date',
+      minWidth: 150,
+    },
+    {
+      Header: (
+        <span className="sortable">
+          {intl.formatMessage({ id: 'doc.expiry_date' })}
+        </span>
+      ),
+      accessor: 'expire-date',
+      minWidth: 150,
     },
     {
       Header: (
@@ -96,31 +89,20 @@ function DatabaseTable({
       accessor: 'status',
       minWidth: 150,
       className: 'status',
-      Cell: (attr) => (
-        <span>
-          {intl.formatMessage({
-            id: `observations.status-${attr.value}`,
-          })}
-
-          {[7, 8, 9].includes(attr.value) && (
-            <Tooltip
-              placement="bottom"
-              overlay={
-                <div style={{ maxWidth: 200 }}>
-                  {intl.formatMessage({
-                    id: `observations.status-${attr.value}.info`,
-                  })}
-                </div>
-              }
-              overlayClassName="c-tooltip no-pointer-events"
-            >
-              <button className="c-button -icon -primary">
-                <Icon name="icon-info" className="-smaller" />
-              </button>
-            </Tooltip>
-          )}
-        </span>
-      ),
+      Cell: (attr) =>
+        console.log(
+          attr.value,
+          intl,
+          intl.formatMessage({
+            id: attr.value,
+          })
+        ) || (
+          <span>
+            {intl.formatMessage({
+              id: attr.value,
+            })}
+          </span>
+        ),
     },
     {
       Header: (
@@ -150,18 +132,6 @@ function DatabaseTable({
       className: 'description',
       minWidth: 120,
     },
-    {
-      Header: (
-        <span className="sortable">
-          {intl.formatMessage({ id: 'category' })}
-        </span>
-      ),
-      accessor: 'category',
-      headerClassName: '-a-left',
-      className: 'description',
-      minWidth: 120,
-    },
-
     {
       Header: (
         <span className="sortable">
@@ -228,47 +198,6 @@ function DatabaseTable({
     },
     {
       Header: (
-        <span className="sortable">
-          {intl.formatMessage({ id: 'evidence' })}
-        </span>
-      ),
-      accessor: 'evidence',
-      headerClassName: '-a-left',
-      className: 'evidence description',
-      minWidth: 250,
-      Cell: (attr) => (
-        <div className="evidence-item-wrapper">
-          {Array.isArray(attr.value) &&
-            attr.value.map((v) => (
-              <a
-                href={v.attachment.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="evidence-item"
-              >
-                <Icon className="" name="icon-file-empty" />
-              </a>
-            ))}
-
-          {!Array.isArray(attr.value) && (
-            <span className="evidence-item-text">{attr.value}</span>
-          )}
-        </div>
-      ),
-    },
-    {
-      Header: (
-        <span className="sortable">
-          {intl.formatMessage({ id: 'litigation-status' })}
-        </span>
-      ),
-      accessor: 'litigation-status',
-      headerClassName: '-a-left',
-      className: 'litigation-status',
-      minWidth: 250,
-    },
-    {
-      Header: (
         <span className="sortable">{intl.formatMessage({ id: 'detail' })}</span>
       ),
       accessor: 'observation',
@@ -283,59 +212,6 @@ function DatabaseTable({
         >
           {attr.value}
         </ReadMore>
-      ),
-    },
-    {
-      Header: (
-        <span className="sortable">
-          {intl.formatMessage({ id: 'severity' })}
-        </span>
-      ),
-      accessor: 'level',
-      headerClassName: 'severity-th',
-      className: 'severity',
-      Cell: (attr) => {
-        return (
-          <span
-            className={`severity-item -sev-${attr.value}`}
-            // TODO: fix this
-            // style={{ color: PALETTE_COLOR_1[+attr.value].fill }}
-          >
-            {attr.value}
-          </span>
-        );
-      },
-    },
-    {
-      Header: (
-        <span className="sortable">
-          {intl.formatMessage({ id: 'location-accuracy' })}
-        </span>
-      ),
-      accessor: 'location-accuracy',
-      headerClassName: '-a-left',
-      className: 'location-accuracy',
-      minWidth: 250,
-    },
-    {
-      Header: '',
-      accessor: 'location',
-      headerClassName: '',
-      className: 'location',
-      expander: true,
-      // eslint-disable-next-line react/prop-types
-      Expander: ({ isExpanded }) => (
-        <div className="location-item-wrapper">
-          {isExpanded ? (
-            <button className="c-button -small -secondary">
-              <Icon name="icon-cross" />
-            </button>
-          ) : (
-            <button className="c-button -small -primary">
-              <Icon name="icon-location" />
-            </button>
-          )}
-        </div>
       ),
     },
     {
@@ -358,17 +234,14 @@ function DatabaseTable({
     },
   ];
 
-  function getPageSize() {
-    if (observations.data.length) {
-      return observations.data.length > 50 ? 50 : observations.data.length;
-    }
+  const tableOptions = inputs.map((column) => ({
+    label: intl.formatMessage({ id: column }),
+    value: column,
+  }));
 
-    return 1;
-  }
-
-  function onPageChange(p) {
-    setPage(p + 1);
-    _getObservations(p + 1);
+  let pageSize = 1;
+  if (database.data.length) {
+    pageSize = database.data.length > 30 ? 30 : database.data.length;
   }
 
   return (
@@ -379,13 +252,13 @@ function DatabaseTable({
             id: 'select-table-content',
           })}
         </h2>
-        <Spinner isLoading={observations.loading} />
+        <Spinner isLoading={database.loading} />
         <CheckboxGroup
           className="-inline -single-row"
           name="observations-columns"
           onChange={(value) => _setActiveColumns(value)}
           properties={{
-            default: observations.columns,
+            default: database.columns,
             name: 'observations-columns',
           }}
           options={tableOptions}
@@ -396,24 +269,25 @@ function DatabaseTable({
           data={parsedTableDocuments}
           options={{
             columns: columnHeaders.filter((header) =>
-              observations.columns.includes(header.accessor)
+              database.columns.includes(header.accessor)
             ),
-            pageSize: getPageSize(),
+            // page,
+            pageSize,
             pagination: true,
             previousText: '<',
             nextText: '>',
             noDataText: 'No rows found',
             showPageSizeOptions: false,
-            onPageChange: (p) => onPageChange(p),
+            // onPageChange: (p) => setPage(p + 1),
             defaultSorted: [
               {
                 id: 'date',
                 desc: false,
               },
             ],
-            showSubComponent: observations.columns.includes('location'),
+            showSubComponent: database.columns.includes('location'),
             subComponent: (row) =>
-              observations.columns.includes('location') && (
+              database.columns.includes('location') && (
                 <MapSubComponent
                   id={row.original.id}
                   location={row.original.location}
@@ -428,19 +302,18 @@ function DatabaseTable({
 }
 
 DatabaseTable.propTypes = {
-  observations: PropTypes.object,
+  database: PropTypes.object,
   intl: intlShape.isRequired,
   parsedTableDocuments: PropTypes.array,
-  getObservations: PropTypes.func,
   setActiveColumns: PropTypes.func,
 };
 
 export default withIntl(
   connect(
     (state) => ({
-      observations: state.observations,
+      database: state.database,
       parsedTableDocuments: getParsedTableDocuments(state),
     }),
-    { getObservations, setActiveColumns }
+    { setActiveColumns }
   )(DatabaseTable)
 );
