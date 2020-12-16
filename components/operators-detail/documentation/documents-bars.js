a0cb72f2481301b2f5f2b4df07e0bbe78a9f97df
Daniel Fernández Rico <deferrico@gmail.com>
2020-12-16 12:18:24 +0100

    Removed doc_not_required from % bars in documentation area

--- components/operators-detail/documentation/documents-bars.js
+++ components/operators-detail/documentation/documents-bars.js
@@ -1,11 +1,15 @@
 import sortBy from 'lodash/sortBy';
+import omit from 'lodash/omit';
 import { HELPERS_DOC } from 'utils/documentation';
 
 export default function DocumentStatusBar({ category, className, docs }) {
-  const groupedByStatus = HELPERS_DOC.getGroupedByStatus(docs);
-  const validDocs =
-    (groupedByStatus.doc_valid?.length || 0) +
-    (groupedByStatus.doc_not_required?.length || 0);
+  const groupedByStatus = omit(
+    HELPERS_DOC.getGroupedByStatus(docs),
+    'doc_not_required'
+  );
+  const validDocs = groupedByStatus.doc_valid?.length || 0;
+  const totalDocs = docs.filter((doc) => doc.status !== 'doc_not_required');
+
   return (
     <div className={`c-doc-by-category${className ? ` ${className}` : ''}`}>
       <div className="doc-by-category-desc">
@@ -13,7 +17,7 @@ export default function DocumentStatusBar({ category, className, docs }) {
           <div className="doc-by-category-bar">
             {sortBy(Object.keys(groupedByStatus)).map((status) => {
               const segmentWidth = `${
-                (groupedByStatus[status].length / docs.length) * 100
+                (groupedByStatus[status].length / totalDocs.length) * 100
               }%`;
               return (
                 <div
@@ -26,7 +30,7 @@ export default function DocumentStatusBar({ category, className, docs }) {
           </div>
           <span>{`${
             groupedByStatus.doc_valid || groupedByStatus.doc_not_required
-              ? ((validDocs / docs.length) * 100).toFixed(0)
+              ? ((validDocs / totalDocs.length) * 100).toFixed(0)
               : 0
           }% valid`}</span>
         </div>
