da0c075ca605c8ca6ed3af8dca088e30a5ecaf53
Daniel Fernández Rico <deferrico@gmail.com>
2021-01-21 17:50:22 +0100

    WIP - pie chart status bars now have proportional length
    # Conflicts:
    #	components/operators-detail/documentation/documents-bars.js

--- components/operators-detail/documentation/documents-bars.js
+++ components/operators-detail/documentation/documents-bars.js
@@ -1,7 +1,12 @@
 import sortBy from 'lodash/sortBy';
 import { HELPERS_DOC } from 'utils/documentation';
 
-export default function DocumentStatusBar({ category, className, docs }) {
+export default function DocumentStatusBar({
+  category,
+  className,
+  docs,
+  maxDocs,
+}) {
   const groupedByStatus = HELPERS_DOC.getGroupedByStatus(docs);
 
   if (groupedByStatus.doc_not_required?.length) {
@@ -15,13 +20,15 @@ export default function DocumentStatusBar({ category, className, docs }) {
   const validDocs = groupedByStatus.doc_valid?.length || 0;
 
   return (
-    <div className={`c-doc-by-category${className ? ` ${className}` : ''}`}>
+    <div className={`c-doc-by-category ${className || ''}`}>
       <div className="doc-by-category-desc">
         <div className="doc-by-category-chart">
           <div className="doc-by-category-bar">
             {sortBy(Object.keys(groupedByStatus)).map((status) => {
               const segmentWidth = `${
-                (groupedByStatus[status].length / docs.length) * 100
+                (groupedByStatus[status].length / docs.length) *
+                (docs.length / (maxDocs || docs.length)) *
+                100
               }%`;
               return (
                 <div
