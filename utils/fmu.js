91043dd12d6fc94459ed0026d7ab92d54867ae90
Tomasz Subik <tsubik@gmail.com>
2022-04-15 11:27:06 +0200

    remove not used fmu card component

--- utils/fmu.js
+++ utils/fmu.js
@@ -5,34 +5,6 @@ import sortBy from 'lodash/sortBy';
 const JSONA = new Jsona();
 
 const HELPERS_FMU = {
-  getCertifications(fmu) {
-    const fsc = fmu['certification-fsc'];
-    const olb = fmu['certification-olb'];
-    const pefc = fmu['certification-pefc'];
-    const vlo = fmu['certification-vlc'];
-    const vlc = fmu['certification-vlo'];
-    const tltv = fmu['certification-tltv'];
-
-    if (!fsc && !olb && !pefc) {
-      return '-';
-    }
-
-    const certifications = [
-      !!fsc && 'FSC',
-      !!olb && 'OLB',
-      !!pefc && 'PEFC',
-      !!vlc && 'VLC',
-      !!vlo && 'VLO',
-      !!tltv && 'TLTV'
-    ];
-
-    return (
-      <div>
-        {certifications.filter(c => !!c).join(' - ')}
-      </div>
-    );
-  },
-
   getFmusByOperatorId(operatorId, lang) {
     return fetch(`${process.env.OTP_API}/fmus?filter[operator]=${operatorId}&locale=${lang}`, {
       method: 'GET',
@@ -50,7 +22,6 @@ const HELPERS_FMU = {
         return sortBy(dataParsed.map(f => ({ label: f.name, value: f.id })), 'label');
       });
   }
-
 };
 
 export { HELPERS_FMU };
