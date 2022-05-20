615ba3c97525bbc68f016145fb8190ff8e1c99b9
Tomasz Subik <tsubik@gmail.com>
2022-05-20 17:04:39 +0200

    better pluralize some sentences on operator details page

--- components/operators-detail/overview/gallery-1.js
+++ components/operators-detail/overview/gallery-1.js
@@ -18,6 +18,10 @@ function Gallery({
   operatorDocumentation,
   intl,
 }) {
+  const observations = operatorsDetail.data?.observations?.length || 0;
+  const visits = operatorsDetail.data.observations ? HELPERS_OBS.getMonitorVisits(operatorObservations) : 0;
+  const fmus = operatorsDetail.data?.fmus?.length || 0;
+
   return (
     <div className="c-gallery">
       <div className="row l-row">
@@ -62,14 +66,15 @@ function Gallery({
               id: 'operator-detail.overview.card2.title',
             })}
             description={intl.formatMessage(
-              { id: 'operator-detail.overview.card2.description' },
               {
-                observations: operatorsDetail.data.observations
-                  ? operatorsDetail.data.observations.length
-                  : '-',
-                visits: operatorsDetail.data.observations
-                  ? HELPERS_OBS.getMonitorVisits(operatorObservations)
-                  : '-',
+                id: observations > 0 ? 'operator-detail.overview.cardobs.description' : 'operator-detail.overview.cardobs.none',
+                defaultMessage: observations > 0
+                  ? '{observations, plural, one {There was {observations} observation} other {There were {observations} observations}} from {visits, plural, one {{visits} independent monitor visit} other {{visits} independent monitor visits}}.'
+                  : 'There are no observations.'
+              },
+              {
+                observations,
+                visits
               }
             )}
             link={{
@@ -85,18 +90,21 @@ function Gallery({
         <div className="columns small-12 medium-4">
           <Card
             theme="-primary"
-            letter={
-              operatorsDetail.data.fmus ? operatorsDetail.data.fmus.length : '-'
-            }
-            title={intl.formatMessage({
-              id: 'operator-detail.overview.card3.title',
-            })}
-            description={intl.formatMessage(
-              { id: 'operator-detail.overview.card3.description' },
+            letter={operatorsDetail.data.fmus ? operatorsDetail.data.fmus.length : '-'}
+            title={intl.formatMessage(
               {
-                fmus: operatorsDetail.data.fmus
-                  ? operatorsDetail.data.fmus.length
-                  : '-',
+                id: 'operator-detail.overview.cardfmu.title',
+                defaultMessage: '{fmus, plural, one {Forest management unit} other {Forest management units}}'
+              },
+              { fmus }
+            )}
+            description={intl.formatMessage(
+              {
+                id: 'operator-detail.overview.cardfmu.description',
+                defaultMessage: '{fmus, plural, =0 {{fmus} are managed by {company_name}.} one {{fmus} is managed by {company_name}. The map of this FMU is available in the FMUs section} other {{fmus} are managed by {company_name}. The map of these FMUs is available in the FMUs section.}}'
+              },
+              {
+                fmus,
                 company_name: operatorsDetail.data.name || '',
               }
             )}
