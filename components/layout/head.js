e7023d00b831a3b5e813b04bd5cc82a44469980d
Tomasz Subik <tsubik@gmail.com>
2022-07-07 18:25:27 +0200

    update hotjar id

--- components/layout/head.js
+++ components/layout/head.js
@@ -50,7 +50,7 @@ export default class Head extends React.Component {
             dangerouslySetInnerHTML={{__html: `
               (function(h,o,t,j,a,r){
               h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:2890428,hjsv:6};
+              h._hjSettings={hjid:3036814,hjsv:6};
               a=o.getElementsByTagName('head')[0];
               r=o.createElement('script');r.async=1;
               r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
