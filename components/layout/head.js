ecbb6c2eae2a2a1de7c5d23dc492ff13b53fc5d4
Tomasz Subik <tsubik@gmail.com>
2022-08-19 17:36:05 +0200

    upgrade to next 10

--- components/layout/head.js
+++ components/layout/head.js
@@ -1,53 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import HeadNext from 'next/head';
-import styles from 'css/index.scss';
 
-// Styles
+const Head = ({title, description}) => {
+  return (
+    <HeadNext>
+      <title>{title} | Open Timber Portal</title>
+      <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="author" content="Vizzuality" />
 
-export default class Head extends React.Component {
+      {/* Favicon */}
+      <link rel="apple-touch-icon" sizes="57x57" href="/static/favicon/apple-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="60x60" href="/static/favicon/apple-icon-60x60.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/static/favicon/apple-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/static/favicon/apple-icon-76x76.png" />
+      <link rel="apple-touch-icon" sizes="114x114" href="/static/favicon/apple-icon-114x114.png" />
+      <link rel="apple-touch-icon" sizes="120x120" href="/static/favicon/apple-icon-120x120.png" />
+      <link rel="apple-touch-icon" sizes="144x144" href="/static/favicon/apple-icon-144x144.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/static/favicon/apple-icon-152x152.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-icon-180x180.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/static/favicon/android-icon-192x192.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/static/favicon/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png" />
+      <link rel="mask-icon" href="/static/favicon/favicon.svg" color="white" />
+      <link rel="manifest" href="/static/favicon/manifest.json" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="msapplication-TileImage" content="/static/favicon/ms-icon-144x144.png" />
+      <meta name="theme-color" content="#ffffff" />
 
-  static getStyles() {
-    return <style dangerouslySetInnerHTML={{ __html: styles }} />;
-  }
-
-  render() {
-    const { title, description } = this.props;
-
-    return (
-      <HeadNext>
-        <title>{title} | Open Timber Portal</title>
-        <meta name="description" content={description} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="author" content="Vizzuality" />
-
-        {/* Favicon */}
-        <link rel="apple-touch-icon" sizes="57x57" href="/static/favicon/apple-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="60x60" href="/static/favicon/apple-icon-60x60.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/static/favicon/apple-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/static/favicon/apple-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="/static/favicon/apple-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/static/favicon/apple-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="/static/favicon/apple-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/static/favicon/apple-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-icon-180x180.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/static/favicon/android-icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/static/favicon/favicon-96x96.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png" />
-        <link rel="mask-icon" href="/static/favicon/favicon.svg" color="white" />
-        <link rel="manifest" href="/static/favicon/manifest.json" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-TileImage" content="/static/favicon/ms-icon-144x144.png" />
-        <meta name="theme-color" content="#ffffff" />
-
-        {/* Styles and scripts */}
-        {Head.getStyles()}
-        <script src="https://cdn.polyfill.io/v2/polyfill.min.js" />
-        <script src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}&libraries=places`} />
-        {process.env.ENV === 'production' && (
-          <script
-            dangerouslySetInnerHTML={{__html: `
+      {/* Styles and scripts */}
+      <script src="https://cdn.polyfill.io/v2/polyfill.min.js" />
+      <script src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}&libraries=places`} />
+      {process.env.ENV === 'production' && (
+        <script
+          dangerouslySetInnerHTML={{__html: `
               (function(h,o,t,j,a,r){
               h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
               h._hjSettings={hjid:3036814,hjsv:6};
@@ -56,16 +44,16 @@ export default class Head extends React.Component {
               r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
               a.appendChild(r);
               })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            `}}
-          />
-        )}
-      </HeadNext>
-    );
-  }
-
+          `}}
+        />
+      )}
+    </HeadNext>
+  );
 }
 
 Head.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired
 };
+
+export default Head;
--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,8 @@
 /* eslint-disable global-require */
 module.exports = {
-  plugins: [
-    require('postcss-easy-import')({ prefix: '_' }), // keep this first
-    require('autoprefixer')({
-      /* ...options */
-    }), // so imports are auto-prefixed too
-  ],
+  plugins: {
+    'postcss-easy-import': {
+      prefix: '_'
+    }
+  }
 };
