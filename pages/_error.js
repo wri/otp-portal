f45d2ac24c228918041947949afae4696264c923
Tomasz Subik <tsubik@gmail.com>
2022-08-29 19:05:56 +0200

    add sentry

--- pages/_error.js
+++ pages/_error.js
@@ -6,14 +6,23 @@ import PropTypes from 'prop-types';
 import Head from 'next/head';
 import Link from 'next/link';
 
-class ErrorPage extends React.Component {
+import * as Sentry from '@sentry/nextjs';
 
-  static getInitialProps({ res, xhr, url }) {
-    const errorCode = res ? res.statusCode : xhr.status;
-    return { errorCode, url };
+class ErrorPage extends React.Component {
+  static async getInitialProps(contextData) {
+    const { res, xhr, url } = contextData;
+    const statusCode = res ? res.statusCode : xhr.status;
+
+    await Sentry.captureUnderscoreErrorException(contextData);
+
+    return { statusCode, url };
   }
 
   render() {
+    // If you're using a Nextjs version prior to 12.2.1, uncomment this to
+    // compensate for https://github.com/vercel/next.js/issues/8592
+    Sentry.captureUnderscoreErrorException(this.props);
+
     const css = (
       <Head>
         <style>{`
@@ -23,12 +32,12 @@ class ErrorPage extends React.Component {
             color: #444;
             background-color: #eee;
           }
-        `}</style>
+          `}</style>
       </Head>
     );
 
     let response;
-    switch (this.props.errorCode) {
+    switch (this.props.statusCode) {
       case 200: // Also display a 404 if someone requests /_error explicitly
       case 404:
         response = (
@@ -53,9 +62,9 @@ class ErrorPage extends React.Component {
         response = (
           <div>
             {css}
-            <h1>HTTP { this.props.errorCode } Error</h1>
+            <h1>HTTP { this.props.statusCode } Error</h1>
             <p>
-              An <strong>HTTP { this.props.errorCode }</strong> error occurred while
+              An <strong>HTTP { this.props.statusCode }</strong> error occurred while
               trying to access <strong>{ this.props.url.pathname }</strong>
             </p>
           </div>
@@ -68,8 +77,8 @@ class ErrorPage extends React.Component {
 }
 
 ErrorPage.propTypes = {
-  errorCode: PropTypes.number.isRequired,
-  url: PropTypes.object.isRequired
+  statusCode: PropTypes.number.isRequired,
+  url: PropTypes.object.isRequired,
 };
 
 export default ErrorPage;
