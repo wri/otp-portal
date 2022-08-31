2bf44e773c61b632a7ce21e2484a5ab30c24e232
Tomasz Subik <tsubik@gmail.com>
2022-08-31 18:17:42 +0200

    bring back old tree cover layer for fmu details - temp solution

--- modules/operators-detail-fmus.js
+++ modules/operators-detail-fmus.js
@@ -1,4 +1,4 @@
-import { LAYERS } from 'constants/layers';
+import { LAYERS, OLD_TREE_COVER_LOSS } from 'constants/layers';
 
 const GET_FMU_ANALYSIS_SUCCESS = 'GET_FMU_ANALYSIS_SUCCESS';
 const GET_FMU_ANALYSIS_LOADING = 'GET_FMU_ANALYSIS_LOADING';
@@ -28,7 +28,11 @@ const initialState = {
 
   hoverInteractions: {},
 
-  layers: LAYERS,
+  layers: [
+    ...LAYERS.filter(x => x.id !== 'loss'),
+    // show old tree cover loss layer until new analysis is fixed and deployed
+    OLD_TREE_COVER_LOSS
+  ],
   layersActive: [
     'gain',
     'loss',
