1119250000d747bf22ac73dd2440e16a60b4a388
Tomasz Subik <tsubik@gmail.com>
2022-11-18 12:13:30 +0100

    add country dropdown to the navigation list

--- components/ui/navigation-list.js
+++ components/ui/navigation-list.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
@@ -12,136 +13,150 @@ import LanguageDropdown from 'components/ui/language-dropdown';
 
 import { injectIntl, intlShape } from 'react-intl';
 
-class NavigationList extends React.Component {
-  setActive(pathname) {
-    const { url } = this.props;
+function NavigationList({ hideActive, intl, url, className, countries }) {
+  const setActive = (pathname) => {
     return classnames({
       '-active': pathname.includes(url.pathname),
     });
   }
+  const classNames = classnames({
+    'c-navigation-list': true,
+    [className]: !!className,
+  });
 
-  render() {
-    const { hideActive, className } = this.props;
-    const classNames = classnames({
-      'c-navigation-list': true,
-      [className]: !!className,
-    });
-
-    return (
-      <ul className={classNames}>
-        {hideActive && (
-          <li>
-            <LanguageDropdown />
-          </li>
-        )}
-        {process.env.FEATURE_COUNTRY_PAGES === 'true' && (
-          <li>
-            <Link href="/countries" prefetch={false}>
-              <a
-                className={
-                !hideActive
-                  ? this.setActive(['/countries', '/countries-detail'])
-                  : ''
-                }
-              >
-                {this.props.intl.formatMessage({ id: 'countries' })}
-              </a>
-            </Link>
-          </li>
-        )}
+  return (
+    <ul className={classNames}>
+      {hideActive && (
         <li>
-          <Dropdown
-            className="c-account-dropdown"
-            ref={(d) => {
-              this.dropdown = d;
-            }}
-          >
+          <LanguageDropdown />
+        </li>
+      )}
+      {process.env.FEATURE_COUNTRY_PAGES === 'true' && (
+        <li>
+          <Dropdown className="c-account-dropdown">
             <DropdownTrigger>
               <div
                 className={classnames(
                   'header-nav-list-item',
-                  !hideActive ? this.setActive(['/operators', '/database']) : ''
+                  !hideActive ? setActive(['/countries']) : ''
                 )}
               >
                 <span>
-                  {this.props.intl.formatMessage({ id: 'operators' })}
+                  {intl.formatMessage({ id: 'countries' })}
                 </span>
               </div>
             </DropdownTrigger>
 
             <DropdownContent>
               <ul className="header-dropdown-list">
-                <li className="header-dropdown-list-item">
-                  <Link href="/operators" prefetch={false}>
-                    <a>
-                      {this.props.intl.formatMessage({
-                        id: 'transparency_ranking',
-                      })}
-                    </a>
-                  </Link>
-                </li>
-                <li className="header-dropdown-list-item">
-                  <Link href="/database" prefetch={false}>
-                    <a>
-                      {this.props.intl.formatMessage({
-                        id: 'producers_documents_database',
-                      })}
-                    </a>
-                  </Link>
-                </li>
+                {countries.data.map((country) => (
+                  <li className="header-dropdown-list-item">
+                    <Link href={`/countries/${country.id}`} prefetch={false}>
+                      <a>
+                        {country.name}
+                      </a>
+                    </Link>
+                  </li>
+                ))}
               </ul>
             </DropdownContent>
           </Dropdown>
         </li>
+      )}
+      <li>
+        <Dropdown className="c-account-dropdown">
+          <DropdownTrigger>
+            <div
+              className={classnames(
+                'header-nav-list-item',
+                !hideActive ? setActive(['/operators', '/database']) : ''
+              )}
+            >
+              <span>
+                {intl.formatMessage({ id: 'operators' })}
+              </span>
+            </div>
+          </DropdownTrigger>
+
+          <DropdownContent>
+            <ul className="header-dropdown-list">
+              <li className="header-dropdown-list-item">
+                <Link href="/operators" prefetch={false}>
+                  <a>
+                    {intl.formatMessage({
+                      id: 'transparency_ranking',
+                    })}
+                  </a>
+                </Link>
+              </li>
+              <li className="header-dropdown-list-item">
+                <Link href="/database" prefetch={false}>
+                  <a>
+                    {intl.formatMessage({
+                      id: 'producers_documents_database',
+                    })}
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </DropdownContent>
+        </Dropdown>
+      </li>
+      <li>
+        <Link href="/observations" prefetch={false}>
+          <a className={!hideActive ? setActive(['/observations']) : ''}>
+            {intl.formatMessage({ id: 'observations' })}
+          </a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/help" prefetch={false}>
+          <a className={!hideActive ? setActive(['/help']) : ''}>
+            {intl.formatMessage({ id: 'help' })}
+          </a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about" prefetch={false}>
+          <a className={!hideActive ? setActive(['/about']) : ''}>
+            {intl.formatMessage({ id: 'about' })}
+          </a>
+        </Link>
+      </li>
+      {hideActive && (
         <li>
-          <Link href="/observations" prefetch={false}>
-            <a className={!hideActive ? this.setActive(['/observations']) : ''}>
-              {this.props.intl.formatMessage({ id: 'observations' })}
+          <Link href="/terms" prefetch={false}>
+            <a className={!hideActive ? setActive(['/terms']) : ''}>
+              {intl.formatMessage({ id: 'terms' })}
             </a>
           </Link>
         </li>
+      )}
+      {hideActive && (
         <li>
-          <Link href="/help" prefetch={false}>
-            <a className={!hideActive ? this.setActive(['/help']) : ''}>
-              {this.props.intl.formatMessage({ id: 'help' })}
+          <Link href="/newsletter" prefetch={false}>
+            <a className={!hideActive ? setActive(['/newsletter']) : ''}>
+              {intl.formatMessage({ id: 'newsletter' })}
             </a>
           </Link>
         </li>
-        <li>
-          <Link href="/about" prefetch={false}>
-            <a className={!hideActive ? this.setActive(['/about']) : ''}>
-              {this.props.intl.formatMessage({ id: 'about' })}
-            </a>
-          </Link>
-        </li>
-        {hideActive && (
-          <li>
-            <Link href="/terms" prefetch={false}>
-              <a className={!hideActive ? this.setActive(['/terms']) : ''}>
-                {this.props.intl.formatMessage({ id: 'terms' })}
-              </a>
-            </Link>
-          </li>
-        )}
-        {hideActive && (
-          <li>
-            <Link href="/newsletter" prefetch={false}>
-              <a className={!hideActive ? this.setActive(['/newsletter']) : ''}>
-                {this.props.intl.formatMessage({ id: 'newsletter' })}
-              </a>
-            </Link>
-          </li>
-        )}
-      </ul>
-    );
-  }
+      )}
+    </ul>
+  );
 }
 
 NavigationList.propTypes = {
-  url: PropTypes.object,
-  hideActive: PropTypes.bool,
   className: PropTypes.string,
+  countries: PropTypes.object,
+  hideActive: PropTypes.bool,
   intl: intlShape.isRequired,
+  url: PropTypes.object,
 };
 
-export default injectIntl(NavigationList);
+export default injectIntl(
+  connect(
+    (state) => ({
+      countries: state.countries,
+    }),
+  )(NavigationList)
+);
