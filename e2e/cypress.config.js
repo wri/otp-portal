aa6753ba04fa79b80ba9fcc37708159a539ea1f4
Tomasz Subik <tsubik@gmail.com>
2022-12-07 14:07:08 +0100

    e2e: disable video

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   watchForFileChanges: false,
+  video: false,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
