fc01e47bad5b26fce0a7046f9fa54841d7ade403
Tomasz Subik <tsubik@gmail.com>
2023-01-10 17:09:52 +0100

    country overview vpas and links changes

--- components/countries-detail/overview.js
+++ components/countries-detail/overview.js
@@ -10,54 +10,61 @@ import { injectIntl, intlShape } from 'react-intl';
 function OperatorsDetailOverview(props) {
   const { countriesDetail } = props;
 
+  const vpas = sortBy(countriesDetail.vpas.data, 'position');
+  const links = sortBy(countriesDetail.links.data, 'position');
+
   return (
     <div
       className="c-section"
     >
       <div className="l-container">
-        <article className="c-article">
-          <header>
-            <h2 className="c-title">
-              {props.intl.formatMessage({ id: 'country-detail.vpas.title' })}
-            </h2>
-          </header>
+        {(vpas || []).length > 0 && (
+          <article className="c-article">
+            <header>
+              <h2 className="c-title -extrabig">
+                {props.intl.formatMessage({ id: 'country-detail.vpas.title' })}
+              </h2>
+            </header>
 
-          <div className="content">
-            <ul>
-              {sortBy(countriesDetail.vpas.data, 'position').map(link => (
-                <li
-                  key={link.id}
-                >
-                  <a target="_blank" href={link.url} rel="noopener noreferrer">
-                    {link.name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </article>
+            <div className="content">
+              <ul>
+                {vpas.map(link => (
+                  <li
+                    key={link.id}
+                  >
+                    <a target="_blank" href={link.url} rel="noopener noreferrer">
+                      {link.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </article>
+        )}
 
-        <article className="c-article">
-          <header>
-            <h2 className="c-title">
-              {props.intl.formatMessage({ id: 'country-detail.links.title' })}
-            </h2>
-          </header>
+        {(links || []).length > 0 && (
+          <article className="c-article">
+            <header>
+              <h2 className="c-title -extrabig">
+                {props.intl.formatMessage({ id: 'country-detail.links.title' })}
+              </h2>
+            </header>
 
-          <div className="content">
-            <ul>
-              {sortBy(countriesDetail.links.data, 'position').map(link => (
-                <li
-                  key={link.id}
-                >
-                  <a target="_blank" href={link.url} rel="noopener noreferrer">
-                    {link.name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </article>
+            <div className="content">
+              <ul>
+                {links.map(link => (
+                  <li
+                    key={link.id}
+                  >
+                    <a target="_blank" href={link.url} rel="noopener noreferrer">
+                      {link.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </article>
+        )}
       </div>
     </div>
   );
