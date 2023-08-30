be689e841fdeec7ff68f63549923d6937741801e
Tomasz Subik <tsubik@gmail.com>
2023-08-30 14:28:47 +0200

    upgrade sitemap generation to add alternate links to pages in different languages

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,77 +1,51 @@
 import Jsona from 'jsona';
 import API from 'services/api';
+import { LOCALES } from 'constants/locales';
 
 const JSONA = new Jsona();
 const URL = process.env.APP_URL;
 
 function generateSiteMap(operators, countries) {
-  return `<?xml version="1.0" encoding="UTF-8"?>
-   <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
-     <url>
-       <loc>${URL}</loc>
-     </url>
-     <url>
-       <loc>${URL}/signup</loc>
-     </url>
+  const url = (path) => {
+    return `
+    <url>
+      <loc>${URL}${path}</loc>
+      <xhtml:link rel="alternate" hreflang="en" href="${URL}${path}" />
+      ${LOCALES.filter(l => l.code !== 'en').map(({ code }) => (
+        `<xhtml:link rel="alternate" hreflang="${code}" href="${URL}/${code}${path}" />`
+      )).join("")}
+    </url>
+    `
+  };
+  const paths = [
+    '',
+    '/signup',
+    '/database',
+    '/observations',
+    '/about',
+    '/newsletter',
+    '/terms',
+    '/help/overview',
+    '/help/how-otp-works',
+    '/help/legislation-and-regulations',
+    '/help/faqs',
+    '/help/tutorials',
+    '/operators',
+    '/operators/new',
+    ...operators.flatMap(({ slug }) => [
+      `/operators/${slug}/overview`,
+      `/operators/${slug}/documentation`,
+      `/operators/${slug}/observations`,
+      `/operators/${slug}/fmus`,
+    ]),
+    ...countries.flatMap(({ id }) => [
+      `/countries/${id}`
+    ])
+  ];
 
-     <url>
-       <loc>${URL}/database</loc>
-     </url>
-     <url>
-       <loc>${URL}/observations</loc>
-     </url>
-     <url>
-       <loc>${URL}/about</loc>
-     </url>
-     <url>
-       <loc>${URL}/newsletter</loc>
-     </url>
-     <url>
-       <loc>${URL}/terms</loc>
-     </url>
-     <url>
-       <loc>${URL}/help/overview</loc>
-     </url>
-     <url>
-       <loc>${URL}/help/how-otp-works</loc>
-     </url>
-     <url>
-       <loc>${URL}/help/legislation-and-regulations</loc>
-     </url>
-     <url>
-       <loc>${URL}/help/faqs</loc>
-     </url>
-     <url>
-       <loc>${URL}/help/tutorials</loc>
-     </url>
-     <url>
-       <loc>${URL}/operators</loc>
-     </url>
-     <url>
-       <loc>${URL}/operators/new</loc>
-     </url>
-     ${operators.map(({ slug }) => {
-       return `
-         <url>
-           <loc>${URL}/operators/${slug}/overview</loc>
-         </url>
-         <url>
-           <loc>${URL}/operators/${slug}/documentation</loc>
-         </url>
-         <url>
-           <loc>${URL}/operators/${slug}/observations</loc>
-         </url>
-         <url>
-           <loc>${URL}/operators/${slug}/fmus</loc>
-         </url>
-       `;}).join("")}
-     ${countries.map(({ id }) => {
-       return `
-         <url>
-           <loc>${URL}/countries/${id}</loc>
-         </url>
-       `;
-     }).join("")}
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="https://www.w3.org/1999/xhtml">
+     ${paths.map((path) => url(path)).join("")}
    </urlset>
  `;
 }
