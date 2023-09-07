2d815bd5f3d04db3d1512eb54ca0ac3a2d634d36
Tomasz Subik <tsubik@gmail.com>
2023-09-07 22:41:54 +0200

    another fix to sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -46,10 +46,8 @@ function generateSiteMap(operators, countries) {
 
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset
-     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-     xmlns:xhtml="http://www.w3.org/TR/xhtml11/xhtml11_schema.html"
      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-     xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.w3.org/TR/xhtml11/xhtml11_schema.html http://www.w3.org/2002/08/xhtml/xhtml1-strict.xsd"
+     xmlns:xhtml="http://www.w3.org/1999/xhtml"
    >
      ${paths.map((path) => url(path)).join("")}
    </urlset>
