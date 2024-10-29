116e7e7ba081fe3d36549a15107b4460cbb0f6d7
Tomasz Subik <tsubik@gmail.com>
2024-10-29 15:40:59 +0100

    load dayjs locales asynchronously

--- pages/_app.js
+++ pages/_app.js
@@ -24,15 +24,19 @@ import Error from 'pages/_error';
 
 import { getSession } from 'services/session';
 
-import 'dayjs/locale/fr';
-import 'dayjs/locale/pt';
-import 'dayjs/locale/ja';
-import 'dayjs/locale/ko';
-import 'dayjs/locale/vi';
-import 'dayjs/locale/zh-cn';
-
 import 'css/index.scss';
 import { WebVitalsTracking } from '~/components/layout/web-vitals-tracking';
+
+// workaround as import(`dayjs/locale/${locale}`) was not working
+const loadLocales = {
+  en: () => Promise.resolve(),
+  fr: () => import('dayjs/locale/fr'),
+  pt: () => import('dayjs/locale/pt'),
+  ja: () => import('dayjs/locale/ja'),
+  ko: () => import('dayjs/locale/ko'),
+  vi: () => import('dayjs/locale/vi'),
+  zh: () => import('dayjs/locale/zh-cn')
+}
 
 const reducer = combineReducers({
   ...reducers
@@ -81,6 +85,8 @@ class MyApp extends App {
 
     const languageFile = language === 'zh' ? 'zh_CN' : language;
     const messages = await import(`lang/${languageFile}.json`);
+
+    await loadLocales[language]();
 
     store.dispatch(setLanguage(language));
     store.dispatch(setUser(user));
