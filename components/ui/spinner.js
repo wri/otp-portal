21d67a807f88499f9337f030e660a6a4371604c2
Tomasz Subik <tsubik@gmail.com>
2024-11-06 13:20:11 +0100

    use delayed spinner for better ux

--- components/ui/spinner.js
+++ components/ui/spinner.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import { useSpinDelay } from 'spin-delay';
 
-export default function Spinner({ isLoading, className }) {
+export default function Spinner({ isLoading, className, spinDelay = 500, spinMinDuration = 300 }) {
   const classNames = classnames({
     [className]: !!className,
-    '-loading': isLoading
   });
+  const showSpinner = useSpinDelay(isLoading, { delay: spinDelay, minDuration: spinMinDuration, ssr: false });
+
+  if (!showSpinner) return null;
 
   return (
     <div className={`c-spinner ${classNames}`}>
@@ -19,5 +22,7 @@ export default function Spinner({ isLoading, className }) {
 
 Spinner.propTypes = {
   className: PropTypes.string,
-  isLoading: PropTypes.bool
+  isLoading: PropTypes.bool,
+  spinDelay: PropTypes.number,
+  spinMinDuration: PropTypes.number
 };
