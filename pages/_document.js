b0398e61bff062ea30cdb0991d98b3c35182a26f
Tomasz Subik <tsubik@gmail.com>
2024-11-19 16:30:10 +0100

    preconnect to 3rd party domains

--- pages/_document.js
+++ pages/_document.js
@@ -15,12 +15,19 @@ class MyDocument extends Document {
 
   render() {
     const { language } = this.props;
+    const withOsano = !!process.env.OSANO_ID;
+    const withGTM = !!process.env.GOOGLE_TAG_MANAGER_KEY;
+    const withHotjar = process.env.DISABLE_HOTJAR !== 'true' && process.env.ENV === 'production';
 
     return (
       <Html lang={language}>
-        <Head />
+        <Head>
+          {withOsano && <link rel="preconnect" href="https://cmp.osano.com" />}
+          {withGTM && <link rel="preconnect" href="https://www.googletagmanager.com" />}
+          {withHotjar && <link rel="preconnect" href="https://static.hotjar.com" />}
+        </Head>
         <body>
-          {process.env.OSANO_ID && <Script src={`https://cmp.osano.com/${process.env.OSANO_ID}/osano.js`} strategy="afterInteractive" />}
+          {withOsano && <Script src={`https://cmp.osano.com/${process.env.OSANO_ID}/osano.js`} strategy="beforeInteractive" />}
           <Script src={`/${language === 'en' ? '' : language + '/'}translations.js`} strategy="beforeInteractive" />
           <GoogleTagManager noscript />
           <Main />
