fecf6d7f952f135cb9a0f4ea82cb5db12ccb7351
Tomasz Subik <tsubik@gmail.com>
2025-03-07 10:35:12 +0100

    enable tracing

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -10,7 +10,7 @@ Sentry.init({
   dsn: SENTRY_DSN,
   environment: process.env.ENV,
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 0,
+  tracesSampleRate: 0.4,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
