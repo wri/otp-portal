3f967e46d017a948e072b0d45633c8e4a8ad0975
Tomasz Subik <tsubik@gmail.com>
2025-07-14 11:15:08 +0200

    changes for FMU certificates

--- constants/fmu.js
+++ constants/fmu.js
@@ -1,9 +1,10 @@
 export const CERTIFICATIONS = [
-  { label: 'FSC', value: 'fsc' },
-  { label: 'PEFC', value: 'pefc' },
-  { label: 'OLB', value: 'olb' },
-  { label: 'FSC-CW', value: 'fsc-cw' },
-  { label: 'PAFC', value: 'pafc' },
-  { label: 'TLV', value: 'tlv' },
-  { label: 'LS', value: 'ls' },
+  { label: 'Forest Stewardship Council (FSC)', abbr: 'FSC', value: 'fsc' },
+  { label: 'Programme for the Endorsement of Forest Certification (PEFC)', abbr: 'PEFC', value: 'pefc' },
+  { label: 'Origin and Legality of Wood (OLB)', abbr: 'OLB', value: 'olb' },
+  { label: 'Forest Stewardship Council - Controlled Wood (FSC-CW)', abbr: 'FSC-CW', value: 'fsc-cw' },
+  { label: 'Pan African Forest Certification (PAFC)', abbr: 'PAFC', value: 'pafc' },
+  { label: 'Timber Legality Verification (TLV)', abbr: 'TLV', value: 'tlv' },
+  { label: 'LegalSource (LS)', abbr: 'LS', value: 'ls' },
+  { label: 'Preferred by Nature Certification Regulatory Scope (PbN)', abbr: 'PbN', value: 'pbn' }
 ];
