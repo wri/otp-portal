f264f62247f9059453f9181b523d6dde9a923b35
Tomasz Subik <tsubik@gmail.com>
2025-07-18 09:17:28 +0200

    change fmu certificates order, show certificates in analyze as list

--- constants/fmu.js
+++ constants/fmu.js
@@ -1,10 +1,10 @@
 export const CERTIFICATIONS = [
   { label: 'Forest Stewardship Council (FSC)', abbr: 'FSC', value: 'fsc' },
-  { label: 'Programme for the Endorsement of Forest Certification (PEFC)', abbr: 'PEFC', value: 'pefc' },
-  { label: 'Origin and Legality of Wood (OLB)', abbr: 'OLB', value: 'olb' },
   { label: 'Forest Stewardship Council - Controlled Wood (FSC-CW)', abbr: 'FSC-CW', value: 'fsc-cw' },
-  { label: 'Pan African Forest Certification (PAFC)', abbr: 'PAFC', value: 'pafc' },
-  { label: 'Timber Legality Verification (TLV)', abbr: 'TLV', value: 'tlv' },
   { label: 'LegalSource (LS)', abbr: 'LS', value: 'ls' },
-  { label: 'Preferred by Nature Certification Regulatory Scope (PbN)', abbr: 'PbN', value: 'pbn' }
+  { label: 'Origin and Legality of Wood (OLB)', abbr: 'OLB', value: 'olb' },
+  { label: 'Pan African Forest Certification (PAFC)', abbr: 'PAFC', value: 'pafc' },
+  { label: 'Preferred by Nature Certification Regulatory Scope (PbN)', abbr: 'PbN', value: 'pbn' },
+  { label: 'Programme for the Endorsement of Forest Certification (PEFC)', abbr: 'PEFC', value: 'pefc' },
+  { label: 'Timber Legality Verification (TLV)', abbr: 'TLV', value: 'tlv' }
 ];
