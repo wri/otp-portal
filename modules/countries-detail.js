c2722d4169cb027676fea0c67acafc6bf4516790
Tomasz Subik <tsubik@gmail.com>
2025-08-25 14:53:04 +0200

    refactor modules/countries-detail

--- modules/countries-detail.js
+++ modules/countries-detail.js
@@ -1,66 +1,42 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { addApiCases, createApiInitialState } from 'utils/redux-helpers';
-import API from 'services/api';
+import { createSlice } from '@reduxjs/toolkit';
+import { addApiCases, createApiThunk, createApiInitialState } from 'utils/redux-helpers';
 
-export const getCountry = createAsyncThunk(
+export const getCountry = createApiThunk(
   'countriesDetail/getCountry',
-  async (id, { getState, rejectWithValue }) => {
-    try {
-      const { user, language } = getState();
-      const includeFields = [
+  (id) => `countries/${id}`,
+  {
+    useUserToken: true,
+    params: {
+      include: [
         'governments',
         'required-gov-documents',
         'required-gov-documents.required-gov-document-group',
         'required-gov-documents.required-gov-document-group.parent',
         'required-gov-documents.gov-documents'
-      ];
-
-      const queryParams = {
-        ...(!!includeFields.length && { include: includeFields.join(',') }),
-        locale: language
-      };
-
-      const { data } = await API.get(`countries/${id}`, queryParams, { token: user.token });
-      return data;
-    } catch (err) {
-      return rejectWithValue(err.message);
+      ].join(',')
     }
   }
 );
 
-export const getCountryLinks = createAsyncThunk(
+export const getCountryLinks = createApiThunk(
   'countriesDetail/getCountryLinks',
-  async (id, { getState, rejectWithValue }) => {
-    try {
-      const { user, language } = getState();
-      const queryParams = {
-        country: id,
-        locale: language
-      };
-
-      const { data } = await API.get('country-links', queryParams, { token: user.token });
-      return data;
-    } catch (err) {
-      return rejectWithValue(err.message);
-    }
+  'country-links',
+  {
+    useUserToken: true,
+    params: (id) => ({
+      country: id
+    })
   }
 );
 
-export const getCountryVPAs = createAsyncThunk(
+export const getCountryVPAs = createApiThunk(
   'countriesDetail/getCountryVPAs',
-  async (id, { getState, rejectWithValue }) => {
-    try {
-      const { user, language } = getState();
-      const queryParams = {
-        country: id,
-        locale: language
-      };
-
-      const { data } = await API.get('country-vpas', queryParams, { token: user.token });
-      return data;
-    } catch (err) {
-      return rejectWithValue(err.message);
-    }
+  'country-vpas',
+  {
+    useUserToken: true,
+    params: (id) => ({
+      country: id
+    })
   }
 );
 
