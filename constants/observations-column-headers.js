7ef6f256a2800ed805421d972efac4b63757da82
Tomasz Subik <tsubik@gmail.com>
2026-02-18 10:54:50 +0100

    small updates for observation table headers

--- constants/observations-column-headers.js
+++ constants/observations-column-headers.js
@@ -33,7 +33,7 @@ export function getColumnHeaders(intl) {
         <span className="sortable">{intl.formatMessage({ id: 'date' })}</span>
       ),
       accessor: 'date',
-      minWidth: 75,
+      width: 75
     },
     {
       Header: (
@@ -292,6 +292,7 @@ export function getColumnHeaders(intl) {
       accessor: 'report',
       headerClassName: '',
       className: 'report',
+      width: 75,
       Cell: (attr) => (
         <div className="report-item-wrapper">
           {attr.value ? (
