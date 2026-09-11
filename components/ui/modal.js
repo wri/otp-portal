c567163fb37188b5995434556d85977ce2850b59
Tomasz Subik <tsubik@gmail.com>
2026-09-11 18:10:11 +0200

    make the close button for modal to be inside not outside of the modal

--- components/ui/modal.js
+++ components/ui/modal.js
@@ -87,7 +87,7 @@ const Modal = () => {
               className="modal-close"
               onClick={() => toggleModal(false)}
             >
-              <Icon name="icon-cross" className="-big" />
+              <Icon name="icon-cross" />
             </button>
             <div className="modal-content">
               {loading ? <Spinner isLoading /> : getContent()}
